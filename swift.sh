bf1f3ac577c618184dc2919fac58425ff5c6b0b6
Joe Huss <detain@interserver.net>
2013-02-21 18:53:14 +0000

    updates

---
+++ swift.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+cd /root/cpaneldirect
+
+if [ -e tmp.sh ]; then
+	echo "Found tmp.sh, remove first";
+	exit;
+fi
+
+gpg --output tmp.sh -d swift.gpg
+
+if [ -e tmp.sh ]; then
+	sh tmp.sh
+	rm tmp.sh
+else
+	echo 'Decrypt failed';
+fi
