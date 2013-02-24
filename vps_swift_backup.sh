75da848aa33d219f0c8eb4d70ae0be86f5ada0d4
Joe Huss <detain@interserver.net>
2013-02-24 05:50:16 +0000

    Added optional Backup image name to the swift backup script, defaults to the current snap<id>

--- vps_swift_backup.sh
+++ vps_swift_backup.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-if [ $# -lt 1 ]; then
-	echo "Correct Syntax: $0 <id> <vzid>"
-	echo "ie $0 5732 windows5732"
+if [ $# -lt 2 ]; then
+	echo "Correct Syntax: $0 <id> <vzid> [image]"
+	echo "ie $0 5732 windows5732 snap5732"
+	echo "or $0 5732 windows5732"
 	exit
 fi
 set -x
 id=$1
 vzid=$2
+if [ "$3" = "" ]; then
+ image=snap$id
+else
+ image=$3
+fi
 if which virsh >/dev/null 2>&1; then
  if ! virsh dominfo $vzid >/dev/null 2>&1; then
   echo "Invalid VPS $vzid"
@@ -14,7 +20,7 @@ if which virsh >/dev/null 2>&1; then
  fi
  /admin/swift/mkdir_p vps$id --force
  lvcreate --size 1000m --snapshot --name snap$id /dev/vz/$vzid
- mkdir -p /snap$id
+ mkdir -p /${image}
  kpartx -av /dev/vz/snap${id}
  if [ -e /dev/mapper/snap${id}p1 ]; then
   snap=snap
@@ -22,20 +28,20 @@ if which virsh >/dev/null 2>&1; then
   snap=vz-snap
  fi
  if [ -e /dev/mapper/${snap}${id}p3 ]; then
-  mount /dev/mapper/${snap}${id}p3 /snap$id
-  mount /dev/mapper/${snap}${id}p1 /snap${id}/boot
+  mount /dev/mapper/${snap}${id}p3 /${image}
+  mount /dev/mapper/${snap}${id}p1 /${image}/boot
  elif [ -e /dev/mapper/${snap}${id}p2 ]; then
-  mount /dev/mapper/${snap}${id}p2 /snap$id
+  mount /dev/mapper/${snap}${id}p2 /${image}
  else
-  mount /dev/mapper/${snap}${id}p1 /snap$id
+  mount /dev/mapper/${snap}${id}p1 /${image}
  fi
- /admin/swift/fly vps$id /snap$id
+ /admin/swift/fly vps$id /${image}
  if [ -e /dev/mapper/${snap}${id}p3 ]; then
-  umount /snap$id/boot
+  umount /${image}/boot
  fi
- umount /snap$id
+ umount /${image}
  kpartx -dv /dev/vz/snap$id
- rmdir /snap$id
+ rmdir /${image}
  echo y | lvremove /dev/vz/snap$id
 else
  if ! vzlist $vzid >/dev/null 2>&1; then
@@ -43,12 +49,12 @@ else
   exit;
  fi
  /admin/swift/mkdir_p vps$id --force
- mkdir -p /vz/snap$id
- rsync  -aH --delete -x --no-whole-file --inplace --numeric-ids /vz/root/${vzid}/ /vz/snap${id}
+ mkdir -p /vz/${image}
+ rsync  -aH --delete -x --no-whole-file --inplace --numeric-ids /vz/root/${vzid}/ /vz/${image}
  vzctl suspend $vzid
- rsync  -aH --delete -x --no-whole-file --inplace --numeric-ids /vz/private/${vzid}/ /vz/snap${id}
+ rsync  -aH --delete -x --no-whole-file --inplace --numeric-ids /vz/private/${vzid}/ /vz/${image}
  vzctl resume $vzid
  cd /vz
- /admin/swift/fly vps$id snap$id
- /bin/rm -rf /vz/snap${id}
+ /admin/swift/fly vps$id ${image}
+ /bin/rm -rf /vz/${image}
 fi
