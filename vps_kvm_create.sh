dc02b07ded29fc790aedcd167ad4f99a876c97ba
Joe Huss <detain@interserver.net>
2013-03-05 00:47:28 +0000

    minor updates

--- vps_kvm_create.sh
+++ vps_kvm_create.sh
@@ -240,4 +240,5 @@ fi
  /scripts/tclimit $ip
  vnc="$(virsh dumpxml $name |grep -i "graphics type='vnc'" | cut -d\' -f4)"
  /root/cpaneldirect/vps_kvm_screenshot.sh $(($vnc - 5900)) "$url?action=screenshot&name=$name" 
+ /root/cpaneldirect/vps_kvm_screenshot.sh $(($vnc - 5900)) "$url?action=screenshot&name=$name" 
 fi
