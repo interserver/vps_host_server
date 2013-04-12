d7ab9754affdcadf245b2307fa24b10840211218
Joe Huss <detain@interserver.net>
2013-04-12 05:15:15 +0000

    added age check for cron script killing vps_Get_list check after 2 hours

--- qs_cron.sh
+++ qs_cron.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 export PATH="$PATH:/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin:/usr/local/sbin"
+
+function age() {
+   local filename=$1
+   local changed=`stat -c %Y "$filename"`
+   local now=`date +%s`
+   local elapsed
+
+   let elapsed=now-changed
+   echo $elapsed
+}
+
 if [ "$(ps aux| grep 'php qs_cron.php' | grep -v "grep php" |wc -l)" = "0" ]; then
 	php qs_cron.php >> cron.output 2>&1
+else
+	# kill a get list older than 2 hours
+	if [ $(age cron.age) -gt 7200 ]; then
+		if [ "$(ps uax|grep qs_get_list |grep -v grep)" != "" ]; then
+			kill -9 $(ps uax|grep qs_get_list |grep -v grep | awk '{ print $2 }')
+		fi
+	fi
 fi
