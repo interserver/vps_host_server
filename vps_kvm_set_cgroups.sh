ebe17abbc1b70ee3108d4283d1932daa603bc468
Joe Huss <detain@interserver.net>
2013-08-22 11:04:27 +0000

    couple inor tweaks to the set cgroups code

--- vps_kvm_set_cgroups.sh
+++ vps_kvm_set_cgroups.sh
@@ -6,11 +6,15 @@ IFS="
 if [ ! -d /cgroup/blkio/libvirt/qemu ]; then
 	echo "CGroups Not Detected, Bailing"
 else
-	for i in $(grep -i '<memory ' /etc/libvirt/qemu/* | cut -d/ -f5 | tr '>' ' ' | tr '<' ' ' | tr \. ' ' | awk '{ print $1 " " $5 }'); do
+	for i in $(grep -i '<memory ' /etc/libvirt/qemu/*xml | cut -d/ -f5 | tr '>' ' ' | tr '<' ' ' | tr \. ' ' | awk '{ print $1 " " $5 }'); do
 		vps="$(echo $i | cut -d" " -f1)";  mem="$(echo $i | cut -d" " -f2)";
 		mem="$(echo $mem / 1000 |bc -l | cut -d\. -f1)";
 		memtxt="${mem}Mb Ram";
-		slices="$(echo $mem / 512 |bc -l | cut -d\. -f1)";
+		if [ "$slices" == "" ] || [ $slices -lt 512 ]; then
+			slices=1
+		else
+			slices="$(echo $mem / 512 |bc -l | cut -d\. -f1)";
+		fi
 		cpushares="$(($slices * 512))";
 		ioweight="$(echo "200 + (50 * $slices)" | bc -l | cut -d\. -f1)";
 		echo "$vps$(printf %$((15-${#vps}))s)${memtxt}$(printf %$((11-${#memtxt}))s) = ${slices}$(printf %$((2-${#slices}))s) Slices -----> IO: $ioweight$(printf %$((6-${#ioweight}))s)CPU: $cpushares";
