a0805144a5bc498a946ef6b9cfa6beeba2974c73
Joe Huss <detain@interserver.net>
2013-08-30 00:16:02 +0000

    added blocking smtp on new kvm installs, blocked 7tags, blocked kvm8 from being used on ny4, properly setting OS on reinstall for kvm linux,

--- vps_kvm_create.sh
+++ vps_kvm_create.sh
@@ -301,4 +301,5 @@ fi
  /root/cpaneldirect/vps_kvm_screenshot.sh "$(($vnc - 5900))" "$url?action=screenshot&name=$name";
  sleep 2s;
  /root/cpaneldirect/vps_kvm_screenshot.sh "$(($vnc - 5900))" "$url?action=screenshot&name=$name";
+ /admin/kvmenable blocksmtp $name
 fi;
