ae738bb735a6c3723dcc5b9d1783a7f8619e10a5
Joe Huss <detain@interserver.net>
2014-03-25 05:30:59 +0000

    New script to get a custom image for a VPS install, so far its meant for KVM installs.
    It will check the filesize of the image using a headers only curl call , and make sure we have enough space,
    then it will download it to a temporary LVM it creates, check the image formaat and convert if necessary
    to raw format then write it to the vps and restart it .

---
+++ vps_get_image.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+export PATH="$PATH:/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin:/usr/local/sbin"
+vps=windows24484
+img="http://cloud-images.ubuntu.com/trusty/current/trusty-server-cloudimg-amd64-uefi1.img"
+destdev=/dev/vz/$vps
+virsh destroy $vps
+#first we get the free disk space on the KVM in G
+p="$(pvdisplay -c)"
+pesize=$(($(echo "$p" | cut -d: -f8) * 1000))
+totalpe="$(echo "$p" | cut -d: -f9)"
+freepe="$(echo "$p" | cut -d: -f10)"
+totalb=$(($pesize*$totalpe))
+totalg=$((${totalb}/1000000000))
+freeb=$(($pesize*$freepe))
+freeg=$((${freeb}/1000000000))
+#next we get the size of the image in G
+echo "LVM  $freeb / $totalb Free"
+imgsize=$(curl -L -s -I "$img" | grep "^Content-Length:" | sed -e s#"\n"#""#g | cut -d" " -f2 | sed s#"\r"#""#g)
+imgbuff=$(echo "$imgsize + 10000000" | bc -l)
+if [ "$imgsize" == "" ]; then
+	echo "Error with $img"
+	echo "headers are"
+	cat curl_headers.txt
+	exit
+fi
+imgbuff=$(($imgsize+10000000))
+if [ $imgbuff -ge $freeb ]; then
+	echo "Not Enough Free Space"
+	echo "Image Size $imgsize"
+	echo "Free Space $freeb ($freeg G / $totalg G)"
+	exit
+fi
+lvsize=$(($(($(($imgbuff/512))+1))*512))
+lvcreate -L${lvsize}B -nimage_storage vz
+mke2fs /dev/vz/image_storage
+mkdir -p /image_storage
+mount /dev/vz/image_storage /image_storage
+curl -L -o /image_storage/image.img "$img"
+format="$(qemu-img info /image_storage/image.img |grep "file format:" | awk '{ print $3 }')"
+if [ "$format" = "raw" ] ; then
+	dd if=/image_storage/image.img of=$destdev
+else
+	qemu-img convert /image_storage/image.img -O raw "$destdev"
+fi
+umount /image_storage
+rmdir /image_storage
+echo y |lvremove /dev/vz/image_storage
+virsh start $vps
+
--- vps_refresh_vnc.sh
+++ vps_refresh_vnc.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 if [ $# -ne 1 ]; then
- echo "Refresh VNC settings for VPS"
- echo "Syntax $0 [vps]"
- echo " ie $0 windows1"
+	echo "Refresh VNC settings for VPS"
+	echo "Syntax $0 [vps]"
+	echo " ie $0 windows1"
 else
- vps="$1"
- id="$(echo "$vps" | sed s#"[a-zA-Z]"#""#g)"
- vnc="$(grep "^$id:" /root/cpaneldirect/vps.vncmap | cut -d: -f2)"
- if [ "$vnc" != "" ]; then
-  /root/cpaneldirect/vps_kvm_setup_vnc.sh $vps $vnc
- fi
+	vps="$1"
+	id="$(echo "$vps" | sed s#"[a-zA-Z]"#""#g)"
+	vnc="$(grep "^$id:" /root/cpaneldirect/vps.vncmap | cut -d: -f2)"
+	if [ "$vnc" != "" ]; then
+		/root/cpaneldirect/vps_kvm_setup_vnc.sh $vps $vnc
+	fi
 fi
 
