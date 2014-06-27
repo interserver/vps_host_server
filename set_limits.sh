7b54a2c7c001ca316904ea6418ab4722090e2695
Joe Huss <detain@interserver.net>
2014-06-27 11:35:04 +0000

    fixs

--- set_limits.sh
+++ set_limits.sh
@@ -27,7 +27,7 @@ if [ -e /cgroup/blkio/libvirt/qemu ] || [ -e $(ls /sys/fs/cgroup/blkio/machine/*
         cgid="$(echo \"\$i\" |  cut -d/ -f7 | sed s#"\.libvirt-qemu$"#""#g)"
     fi
     for i in $(ls $cgall); do
-        id="${cgid}";
+        id="$(eval ${cgid})";
         mem="$(grep -i '<memory ' /etc/libvirt/qemu/${id}.xml |  tr '>' ' ' | tr '<' ' ' | tr \. ' ' | awk '{ print $3 }')";
         mem="$(echo $mem / 1000 |bc -l | cut -d\. -f1)";
         if [ "$mem" == "" ] || [ $mem -lt ${sliceram} ]; then
