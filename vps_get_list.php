42488209553d66875d55d0588850321850635963
Joe Huss <detain@interserver.net>
2014-07-15 23:21:01 +0000

    some updates/adjustments to the code that gets openvz vps info, adding some new fields and ignoring quota checks on ploop

--- vps_get_list.php
+++ vps_get_list.php
@@ -12,127 +12,127 @@
  *              $array =  xml2array(file_get_contents('feed.xml', 1, 'attribute'));
  */
 function xml2array($contents, $get_attributes=1, $priority = 'tag') {
-    if(!$contents) return array();
+	if(!$contents) return array();
 
-    if(!function_exists('xml_parser_create')) {
-        //print "'xml_parser_create()' function not found!";
-        return array();
-    }
+	if(!function_exists('xml_parser_create')) {
+		//print "'xml_parser_create()' function not found!";
+		return array();
+	}
 
-    //Get the XML parser of PHP - PHP must have this module for the parser to work
-    $parser = xml_parser_create('');
-    xml_parser_set_option($parser, XML_OPTION_TARGET_ENCODING, "UTF-8"); # http://minutillo.com/steve/weblog/2004/6/17/php-xml-and-character-encodings-a-tale-of-sadness-rage-and-data-loss
-    xml_parser_set_option($parser, XML_OPTION_CASE_FOLDING, 0);
-    xml_parser_set_option($parser, XML_OPTION_SKIP_WHITE, 1);
-    xml_parse_into_struct($parser, trim($contents), $xml_values);
-    xml_parser_free($parser);
+	//Get the XML parser of PHP - PHP must have this module for the parser to work
+	$parser = xml_parser_create('');
+	xml_parser_set_option($parser, XML_OPTION_TARGET_ENCODING, "UTF-8"); # http://minutillo.com/steve/weblog/2004/6/17/php-xml-and-character-encodings-a-tale-of-sadness-rage-and-data-loss
+	xml_parser_set_option($parser, XML_OPTION_CASE_FOLDING, 0);
+	xml_parser_set_option($parser, XML_OPTION_SKIP_WHITE, 1);
+	xml_parse_into_struct($parser, trim($contents), $xml_values);
+	xml_parser_free($parser);
 
-    if(!$xml_values) return false;//Hmm...
+	if(!$xml_values) return false;//Hmm...
 
-    //Initializations
-    $xml_array = array();
-    $parents = array();
-    $opened_tags = array();
-    $arr = array();
+	//Initializations
+	$xml_array = array();
+	$parents = array();
+	$opened_tags = array();
+	$arr = array();
 
-    $current = &$xml_array; //Refference
+	$current = &$xml_array; //Refference
 
-    //Go through the tags.
-    $repeated_tag_index = array();//Multiple tags with same name will be turned into an array
-    foreach($xml_values as $data) {
-        unset($attributes,$value);//Remove existing values, or there will be trouble
+	//Go through the tags.
+	$repeated_tag_index = array();//Multiple tags with same name will be turned into an array
+	foreach($xml_values as $data) {
+		unset($attributes,$value);//Remove existing values, or there will be trouble
 
-        //This command will extract these variables into the foreach scope
-        // tag(string), type(string), level(int), attributes(array).
-        extract($data);//We could use the array by itself, but this cooler.
+		//This command will extract these variables into the foreach scope
+		// tag(string), type(string), level(int), attributes(array).
+		extract($data);//We could use the array by itself, but this cooler.
 
-        $result = array();
-        $attributes_data = array();
-        
-        if(isset($value)) {
-            if($priority == 'tag') $result = $value;
-            else $result['value'] = $value; //Put the value in a assoc array if we are in the 'Attribute' mode
-        }
+		$result = array();
+		$attributes_data = array();
+		
+		if(isset($value)) {
+			if($priority == 'tag') $result = $value;
+			else $result['value'] = $value; //Put the value in a assoc array if we are in the 'Attribute' mode
+		}
 
-        //Set the attributes too.
-        if(isset($attributes) and $get_attributes) {
-            foreach($attributes as $attr => $val) {
-                if($priority == 'tag') $attributes_data[$attr] = $val;
-                else $result['attr'][$attr] = $val; //Set all the attributes in a array called 'attr'
-            }
-        }
+		//Set the attributes too.
+		if(isset($attributes) and $get_attributes) {
+			foreach($attributes as $attr => $val) {
+				if($priority == 'tag') $attributes_data[$attr] = $val;
+				else $result['attr'][$attr] = $val; //Set all the attributes in a array called 'attr'
+			}
+		}
 
-        //See tag status and do the needed.
-        if($type == "open") {//The starting of the tag '<tag>'
-            $parent[$level-1] = &$current;
-            if(!is_array($current) or (!in_array($tag, array_keys($current)))) { //Insert New tag
-                $current[$tag] = $result;
-                if($attributes_data) $current[$tag. '_attr'] = $attributes_data;
-                $repeated_tag_index[$tag.'_'.$level] = 1;
+		//See tag status and do the needed.
+		if($type == "open") {//The starting of the tag '<tag>'
+			$parent[$level-1] = &$current;
+			if(!is_array($current) or (!in_array($tag, array_keys($current)))) { //Insert New tag
+				$current[$tag] = $result;
+				if($attributes_data) $current[$tag. '_attr'] = $attributes_data;
+				$repeated_tag_index[$tag.'_'.$level] = 1;
 
-                $current = &$current[$tag];
+				$current = &$current[$tag];
 
-            } else { //There was another element with the same tag name
+			} else { //There was another element with the same tag name
 
-                if(isset($current[$tag][0])) {//If there is a 0th element it is already an array
-                    $current[$tag][$repeated_tag_index[$tag.'_'.$level]] = $result;
-                    $repeated_tag_index[$tag.'_'.$level]++;
-                } else {//This section will make the value an array if multiple tags with the same name appear together
-                    $current[$tag] = array($current[$tag],$result);//This will combine the existing item and the new item together to make an array
-                    $repeated_tag_index[$tag.'_'.$level] = 2;
-                    
-                    if(isset($current[$tag.'_attr'])) { //The attribute of the last(0th) tag must be moved as well
-                        $current[$tag]['0_attr'] = $current[$tag.'_attr'];
-                        unset($current[$tag.'_attr']);
-                    }
+				if(isset($current[$tag][0])) {//If there is a 0th element it is already an array
+					$current[$tag][$repeated_tag_index[$tag.'_'.$level]] = $result;
+					$repeated_tag_index[$tag.'_'.$level]++;
+				} else {//This section will make the value an array if multiple tags with the same name appear together
+					$current[$tag] = array($current[$tag],$result);//This will combine the existing item and the new item together to make an array
+					$repeated_tag_index[$tag.'_'.$level] = 2;
+					
+					if(isset($current[$tag.'_attr'])) { //The attribute of the last(0th) tag must be moved as well
+						$current[$tag]['0_attr'] = $current[$tag.'_attr'];
+						unset($current[$tag.'_attr']);
+					}
 
-                }
-                $last_item_index = $repeated_tag_index[$tag.'_'.$level]-1;
-                $current = &$current[$tag][$last_item_index];
-            }
+				}
+				$last_item_index = $repeated_tag_index[$tag.'_'.$level]-1;
+				$current = &$current[$tag][$last_item_index];
+			}
 
-        } elseif($type == "complete") { //Tags that ends in 1 line '<tag />'
-            //See if the key is already taken.
-            if(!isset($current[$tag])) { //New Key
-                $current[$tag] = $result;
-                $repeated_tag_index[$tag.'_'.$level] = 1;
-                if($priority == 'tag' and $attributes_data) $current[$tag. '_attr'] = $attributes_data;
+		} elseif($type == "complete") { //Tags that ends in 1 line '<tag />'
+			//See if the key is already taken.
+			if(!isset($current[$tag])) { //New Key
+				$current[$tag] = $result;
+				$repeated_tag_index[$tag.'_'.$level] = 1;
+				if($priority == 'tag' and $attributes_data) $current[$tag. '_attr'] = $attributes_data;
 
-            } else { //If taken, put all things inside a list(array)
-                if(isset($current[$tag][0]) and is_array($current[$tag])) {//If it is already an array...
+			} else { //If taken, put all things inside a list(array)
+				if(isset($current[$tag][0]) and is_array($current[$tag])) {//If it is already an array...
 
-                    // ...push the new element into that array.
-                    $current[$tag][$repeated_tag_index[$tag.'_'.$level]] = $result;
-                    
-                    if($priority == 'tag' and $get_attributes and $attributes_data) {
-                        $current[$tag][$repeated_tag_index[$tag.'_'.$level] . '_attr'] = $attributes_data;
-                    }
-                    $repeated_tag_index[$tag.'_'.$level]++;
+					// ...push the new element into that array.
+					$current[$tag][$repeated_tag_index[$tag.'_'.$level]] = $result;
+					
+					if($priority == 'tag' and $get_attributes and $attributes_data) {
+						$current[$tag][$repeated_tag_index[$tag.'_'.$level] . '_attr'] = $attributes_data;
+					}
+					$repeated_tag_index[$tag.'_'.$level]++;
 
-                } else { //If it is not an array...
-                    $current[$tag] = array($current[$tag],$result); //...Make it an array using using the existing value and the new value
-                    $repeated_tag_index[$tag.'_'.$level] = 1;
-                    if($priority == 'tag' and $get_attributes) {
-                        if(isset($current[$tag.'_attr'])) { //The attribute of the last(0th) tag must be moved as well
-                            
-                            $current[$tag]['0_attr'] = $current[$tag.'_attr'];
-                            unset($current[$tag.'_attr']);
-                        }
-                        
-                        if($attributes_data) {
-                            $current[$tag][$repeated_tag_index[$tag.'_'.$level] . '_attr'] = $attributes_data;
-                        }
-                    }
-                    $repeated_tag_index[$tag.'_'.$level]++; //0 and 1 index is already taken
-                }
-            }
+				} else { //If it is not an array...
+					$current[$tag] = array($current[$tag],$result); //...Make it an array using using the existing value and the new value
+					$repeated_tag_index[$tag.'_'.$level] = 1;
+					if($priority == 'tag' and $get_attributes) {
+						if(isset($current[$tag.'_attr'])) { //The attribute of the last(0th) tag must be moved as well
+							
+							$current[$tag]['0_attr'] = $current[$tag.'_attr'];
+							unset($current[$tag.'_attr']);
+						}
+						
+						if($attributes_data) {
+							$current[$tag][$repeated_tag_index[$tag.'_'.$level] . '_attr'] = $attributes_data;
+						}
+					}
+					$repeated_tag_index[$tag.'_'.$level]++; //0 and 1 index is already taken
+				}
+			}
 
-        } elseif($type == 'close') { //End of tag '</tag>'
-            $current = &$parent[$level-1];
-        }
-    }
-    
-    return($xml_array);
+		} elseif($type == 'close') { //End of tag '</tag>'
+			$current = &$parent[$level-1];
+		}
+	}
+	
+	return($xml_array);
 }  
 
 	/**
@@ -214,12 +214,12 @@ $cmd .= "./vncsnapshot -dieblank -compresslevel 0 -quality 70 -vncQuality 7 -jpe
 		}
 		else
 		{
-			$out = `export PATH="\$PATH:/bin:/usr/bin:/sbin:/usr/sbin";vzlist -a -o veid,numproc,status,ip,hostname,kmemsize,kmemsize.f,lockedpages,lockedpages.f,privvmpages,privvmpages.f,shmpages,shmpages.f,numproc.f,physpages,physpages.f,vmguarpages,vmguarpages.f,oomguarpages,oomguarpages.f,numtcpsock,numtcpsock.f,numflock,numflock.f,numpty,numpty.f,numsiginfo,numsiginfo.f,tcpsndbuf,tcpsndbuf.f,tcprcvbuf,tcprcvbuf.f,othersockbuf,othersockbuf.f,dgramrcvbuf,dgramrcvbuf.f,numothersock,numothersock.f,dcachesize,dcachesize.f,numfile,numfile.f,numiptent,numiptent.f,diskspace,diskspace.s,diskspace.h,diskinodes,diskinodes.s,diskinodes.h,laverage -H`;
-			preg_match_all('/\s+(?P<veid>[^\s]+)\s+(?P<numproc>[^\s]+)\s+(?P<status>[^\s]+)\s+(?P<ip>[^\s]+)\s+(?P<hostname>[^\s]+)\s+(?P<kmemsize>[^\s]+)\s+(?P<kmemsize_f>[^\s]+)\s+(?P<lockedpages>[^\s]+)\s+(?P<lockedpages_f>[^\s]+)\s+(?P<privvmpages>[^\s]+)\s+(?P<privvmpages_f>[^\s]+)\s+(?P<shmpages>[^\s]+)\s+(?P<shmpages_f>[^\s]+)\s+(?P<numproc_f>[^\s]+)\s+(?P<physpages>[^\s]+)\s+(?P<physpages_f>[^\s]+)\s+(?P<vmguarpages>[^\s]+)\s+(?P<vmguarpages_f>[^\s]+)\s+(?P<oomguarpages>[^\s]+)\s+(?P<oomguarpages_f>[^\s]+)\s+(?P<numtcpsock>[^\s]+)\s+(?P<numtcpsock_f>[^\s]+)\s+(?P<numflock>[^\s]+)\s+(?P<numflock_f>[^\s]+)\s+(?P<numpty>[^\s]+)\s+(?P<numpty_f>[^\s]+)\s+(?P<numsiginfo>[^\s]+)\s+(?P<numsiginfo_f>[^\s]+)\s+(?P<tcpsndbuf>[^\s]+)\s+(?P<tcpsndbuf_f>[^\s]+)\s+(?P<tcprcvbuf>[^\s]+)\s+(?P<tcprcvbuf_f>[^\s]+)\s+(?P<othersockbuf>[^\s]+)\s+(?P<othersockbuf_f>[^\s]+)\s+(?P<dgramrcvbuf>[^\s]+)\s+(?P<dgramrcvbuf_f>[^\s]+)\s+(?P<numothersock>[^\s]+)\s+(?P<numothersock_f>[^\s]+)\s+(?P<dcachesize>[^\s]+)\s+(?P<dcachesize_f>[^\s]+)\s+(?P<numfile>[^\s]+)\s+(?P<numfile_f>[^\s]+)\s+(?P<numiptent>[^\s]+)\s+(?P<numiptent_f>[^\s]+)\s+(?P<diskspace>[^\s]+)\s+(?P<diskspace_s>[^\s]+)\s+(?P<diskspace_h>[^\s]+)\s+(?P<diskinodes>[^\s]+)\s+(?P<diskinodes_s>[^\s]+)\s+(?P<diskinodes_h>[^\s]+)\s+(?P<laverage>[^\s]+)/', $out, $matches);
+			$out = `export PATH="\$PATH:/bin:/usr/bin:/sbin:/usr/sbin";vzlist -a -o ctid,numproc,status,ip,hostname,vswap,layout,kmemsize,kmemsize.f,lockedpages,lockedpages.f,privvmpages,privvmpages.f,shmpages,shmpages.f,numproc.f,physpages,physpages.f,vmguarpages,vmguarpages.f,oomguarpages,oomguarpages.f,numtcpsock,numtcpsock.f,numflock,numflock.f,numpty,numpty.f,numsiginfo,numsiginfo.f,tcpsndbuf,tcpsndbuf.f,tcprcvbuf,tcprcvbuf.f,othersockbuf,othersockbuf.f,dgramrcvbuf,dgramrcvbuf.f,numothersock,numothersock.f,dcachesize,dcachesize.f,numfile,numfile.f,numiptent,numiptent.f,diskspace,diskspace.s,diskspace.h,diskinodes,diskinodes.s,diskinodes.h,laverage -H`;
+			preg_match_all('/\s+(?P<ctid>[^\s]+)\s+(?P<numproc>[^\s]+)\s+(?P<status>[^\s]+)\s+(?P<ip>[^\s]+)\s+(?P<hostname>[^\s]+)\s+(?P<vswap>[^\s]+)\s+(?P<layout>[^\s]+)\s+(?P<kmemsize>[^\s]+)\s+(?P<kmemsize_f>[^\s]+)\s+(?P<lockedpages>[^\s]+)\s+(?P<lockedpages_f>[^\s]+)\s+(?P<privvmpages>[^\s]+)\s+(?P<privvmpages_f>[^\s]+)\s+(?P<shmpages>[^\s]+)\s+(?P<shmpages_f>[^\s]+)\s+(?P<numproc_f>[^\s]+)\s+(?P<physpages>[^\s]+)\s+(?P<physpages_f>[^\s]+)\s+(?P<vmguarpages>[^\s]+)\s+(?P<vmguarpages_f>[^\s]+)\s+(?P<oomguarpages>[^\s]+)\s+(?P<oomguarpages_f>[^\s]+)\s+(?P<numtcpsock>[^\s]+)\s+(?P<numtcpsock_f>[^\s]+)\s+(?P<numflock>[^\s]+)\s+(?P<numflock_f>[^\s]+)\s+(?P<numpty>[^\s]+)\s+(?P<numpty_f>[^\s]+)\s+(?P<numsiginfo>[^\s]+)\s+(?P<numsiginfo_f>[^\s]+)\s+(?P<tcpsndbuf>[^\s]+)\s+(?P<tcpsndbuf_f>[^\s]+)\s+(?P<tcprcvbuf>[^\s]+)\s+(?P<tcprcvbuf_f>[^\s]+)\s+(?P<othersockbuf>[^\s]+)\s+(?P<othersockbuf_f>[^\s]+)\s+(?P<dgramrcvbuf>[^\s]+)\s+(?P<dgramrcvbuf_f>[^\s]+)\s+(?P<numothersock>[^\s]+)\s+(?P<numothersock_f>[^\s]+)\s+(?P<dcachesize>[^\s]+)\s+(?P<dcachesize_f>[^\s]+)\s+(?P<numfile>[^\s]+)\s+(?P<numfile_f>[^\s]+)\s+(?P<numiptent>[^\s]+)\s+(?P<numiptent_f>[^\s]+)\s+(?P<diskspace>[^\s]+)\s+(?P<diskspace_s>[^\s]+)\s+(?P<diskspace_h>[^\s]+)\s+(?P<diskinodes>[^\s]+)\s+(?P<diskinodes_s>[^\s]+)\s+(?P<diskinodes_h>[^\s]+)\s+(?P<laverage>[^\s]+)/', $out, $matches);
 			//print_r($matches);
 			$servers = array();
 			// build a list of servers, and then send an update command to make usre that the server has information on all servers
-			foreach ($matches['veid'] as $key => $id)
+			foreach ($matches['ctid'] as $key => $id)
 			{
 				$server = array(
 					'veid' => $id, 
@@ -227,6 +227,8 @@ $cmd .= "./vncsnapshot -dieblank -compresslevel 0 -quality 70 -vncQuality 7 -jpe
 					'status' => $matches['status'][$key], 
 					'ip' => $matches['ip'][$key], 
 					'hostname' => $matches['hostname'][$key], 
+					'vswap' => $matches['vswap'][$key], 
+					'layout' => $matches['layout'][$key], 
 					'kmemsize' => $matches['kmemsize'][$key], 
 					'kmemsize_f' => $matches['kmemsize_f'][$key], 
 					'lockedpages' => $matches['lockedpages'][$key], 
@@ -278,12 +280,15 @@ $cmd .= "./vncsnapshot -dieblank -compresslevel 0 -quality 70 -vncQuality 7 -jpe
 			}
 			foreach ($servers as $id => $server)
 			{
-				$out = trim(`export PATH="\$PATH:/bin:/usr/bin:/sbin:/usr/sbin";vzquota stat $id 2>/dev/null | grep blocks | awk '{ print $2 " " $3 }'`);
-				if ($out != '')
+				if ($servers[$id]['layout'] == 'simfs')
 				{
-					$disk = explode(' ', $out);
-					$servers[$id]['diskused'] = $disk[0];
-					$servers[$id]['diskmax'] = $disk[1];
+					$out = trim(`export PATH="\$PATH:/bin:/usr/bin:/sbin:/usr/sbin";vzquota stat $id 2>/dev/null | grep blocks | awk '{ print $2 " " $3 }'`);
+					if ($out != '')
+					{
+						$disk = explode(' ', $out);
+						$servers[$id]['diskused'] = $disk[0];
+						$servers[$id]['diskmax'] = $disk[1];
+					}
 				}
 			}
 		}
