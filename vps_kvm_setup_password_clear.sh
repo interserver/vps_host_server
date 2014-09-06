fdaee9d786be82a06dfaf6a926626883f424cb72
Joe Huss <detain@interserver.net>
2014-09-06 08:40:53 +0000

    Adding removing the windows updates  pending.xml file if it exists to the password clearing to prevent problems

--- vps_kvm_setup_password_clear.sh
+++ vps_kvm_setup_password_clear.sh
@@ -1,45 +1,49 @@
 #!/bin/bash
-export PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin:/usr/X11R6/bin:/root/bin"
-set -x
+export PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin:/usr/X11R6/bin:/root/bin";
+set -x;
 if [ "$(kpartx 2>&1 |grep sync)" = "" ]; then
-	kpartxopts=""
+	kpartxopts="";
 else
-	kpartxopts="-s"
-fi
-name=$1
+	kpartxopts="-s";
+fi;
+name=$1;
 if [ $# -ne 1 ]; then
- echo "Clear VPS Password"
- echo "Syntax $0 [name] [pass]"
- echo " ie $0 windows1337"
+ echo "Clear VPS Password";
+ echo "Syntax $0 [name] [pass]";
+ echo " ie $0 windows1337";
 #check if vps exists
 elif ! virsh dominfo ${name} >/dev/null 2>&1; then
  echo "VPS ${name} doesnt exists!";
 else
- virsh destroy ${name}
+ virsh destroy ${name};
  echo "Creating Partition Table Links" && \
  /sbin/kpartx $kpartxopts -av /dev/vz/${name} && \
  if [ -e "/dev/mapper/vz-${name}p1" ]; then
-  pname="vz-${name}"
+  pname="vz-${name}";
  else
-  pname="$name"
+  pname="$name";
  fi && \
  mkdir -p /vz/mounts/${pname}p2 && \
- echo "Mounting Partition"
+ echo "Mounting Partition";
  if [ -e /dev/mapper/${pname}p2 ]; then
-  mount /dev/mapper/${pname}p2 /vz/mounts/${pname}p2
+  mount /dev/mapper/${pname}p2 /vz/mounts/${pname}p2;
  else
-  mount /dev/mapper/${pname}p1 /vz/mounts/${pname}p2
- fi
- echo "Clearing Password"
- /root/cpaneldirect/sampasswd -r -u Administrator -v /vz/mounts/${pname}p2/Windows/System32/config/SAM
- echo "Saving Changes"
+  mount /dev/mapper/${pname}p1 /vz/mounts/${pname}p2;
+ fi;
+ if [ -e /vz/mounts/${pname}p2/Windows/winsxs/pending.xml ]; then
+  echo "Removing Pending Updates File";
+  rm -f "/vz/mounts/${pname}p2/Windows/winsxs/pending.xml";
+ fi;
+ echo "Clearing Password";
+ /root/cpaneldirect/sampasswd -r -u Administrator -v /vz/mounts/${pname}p2/Windows/System32/config/SAM;
+ echo "Saving Changes";
  sync;
  sleep 2s;
- umount /vz/mounts/${pname}p2 2>/dev/null
+ umount /vz/mounts/${pname}p2 2>/dev/null;
  sync;
  sleep 1s;
- /sbin/kpartx $kpartxopts -dv /dev/vz/${name}
- echo "Starting VPS"
+ /sbin/kpartx $kpartxopts -dv /dev/vz/${name};
+ echo "Starting VPS";
  virsh start ${name};
- /root/cpaneldirect/vps_refresh_vnc.sh $name
+ /root/cpaneldirect/vps_refresh_vnc.sh $name;
 fi
