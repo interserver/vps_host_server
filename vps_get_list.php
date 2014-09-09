1f1b5c75ad29c837c40fbe73eedf594deebd2ccd
Joe Huss <detain@interserver.net>
2014-09-09 01:37:20 +0000

    fixs to some curl calls

--- vps_get_list.php
+++ vps_get_list.php
@@ -306,8 +306,8 @@ $cmd .= "./vncsnapshot -dieblank -compresslevel 0 -quality 70 -vncQuality 7 -jpe
 				$ips[$parts[0]][] = $ip;
 			}
 		}
-		$cmd = 'curl --connect-timeout 60 --max-time 240 -k -F action=serverlist -F servers="' . base64_encode(gzcompress(serialize($ips), 9)) . '"  -F ips="' . base64_encode(gzcompress(serialize($ips), 9)) . '" ' . $curl_cmd . ' "' . $url . '" 2>/dev/null;';
-//		$cmd = 'curl --connect-timeout 60 --max-time 240 -k -F action=serverlist -F servers="' . base64_encode(gzcompress(serialize($servers), 9)) . '" $curlcmd "' . $url . '" 2>/dev/null;';
+		$cmd = 'curl --connect-timeout 60 --max-time 240 -k -d action=serverlist -d servers="' . base64_encode(gzcompress(serialize($ips), 9)) . '"  -d ips="' . base64_encode(gzcompress(serialize($ips), 9)) . '" ' . $curl_cmd . ' "' . $url . '" 2>/dev/null;';
+//		$cmd = 'curl --connect-timeout 60 --max-time 240 -k -d action=serverlist -d servers="' . base64_encode(gzcompress(serialize($servers), 9)) . '" $curlcmd "' . $url . '" 2>/dev/null;';
 		//echo "CMD: $cmd\n";
 		echo trim(`$cmd`);
 	}
