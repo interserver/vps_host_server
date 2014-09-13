eccebb649e40e8faa09642a1930991e001cec81d
Joe Huss <detain@interserver.net>
2014-09-13 18:05:56 +0000

    updatse

--- vps_get_ip_assignments.sh
+++ vps_get_ip_assignments.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Gets a list of all the IPs used by each VPS and sends them
 export PATH="$PATH:/usr/sbin:/sbin:/bin:/usr/bin";
-if [ "$(which vzctl)" != "" ]; then
+if [ "$(which vzctl 2>/dev/null)" != "" ]; then
 	grep "^IP_ADDRESS" /etc/vz/conf/[0-9]*.conf 2>/dev/null | sed s#"^.*/\([0-9]*\)\.conf:IP_ADDRESS=\"\([0-9\. :a-f\/]*\)\""#"\1 \2"#g;
 fi;
 #Sample output:
