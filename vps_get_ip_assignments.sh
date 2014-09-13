aa89e852c4dfa76a3b520d6c581a1c71d0f3880b
Joe Huss <detain@interserver.net>
2014-09-13 17:23:13 +0000

    updates to the get ip addresses script taking a few more possible situations into accoutn

--- vps_get_ip_assignments.sh
+++ vps_get_ip_assignments.sh
@@ -2,7 +2,8 @@
 # Gets a list of all the IPs used by each VPS and sends them
 export PATH="$PATH:/usr/sbin:/sbin:/bin:/usr/bin";
 if [ "$(which vzctl)" != "" ]; then
-	grep IP_ADDRESS /etc/vz/conf/*conf 2>/dev/null | sed s#"^.*/\([0-9]*\)\.conf:IP_ADDRESS=\"\([0-9\. :a-f\/]*\)\""#"\1 \2"#g;
+	grep "^IP_ADDRESS" /etc/vz/conf/[0-9]*.conf 2>/dev/null | sed s#"^.*/\([0-9]*\)\.conf:IP_ADDRESS=\"\([0-9\. :a-f\/]*\)\""#"\1 \2"#g;
+	grep IP_ADDRESS /etc/vz/conf/[0-9]*.conf 2>/dev/null | sed s#"^.*/\([0-9]*\)\.conf:IP_ADDRESS=\"\([0-9\. :a-f\/]*\)\""#"\1 \2"#g;
 fi;
 #Sample output:
 #100 69.10.46.222 209.159.155.10
