f0b87ae5b4dc5a309aa5fa12abc9640ee6a1486a
Joe Huss <detain@interserver.net>
2014-09-23 18:44:38 +0000

    slight tweak to the vps get list code so it doesnt send updates if there is no thign to send

--- vps_get_list.php
+++ vps_get_list.php
@@ -292,19 +292,22 @@ $cmd .= "./vncsnapshot -dieblank -compresslevel 0 -quality 70 -vncQuality 7 -jpe
 				}
 			}
 			//print_r($servers);
-			$ips = array();
-			$tips = explode("\n", trim(`/root/cpaneldirect/vps_get_ip_assignments.sh`));
-			foreach ($tips as $line)
+			$tips = trim(`/root/cpaneldirect/vps_get_ip_assignments.sh`);
+			if ($tips != '')
 			{
-				$parts = explode(' ', $line);
-				$ips[$parts[0]] = array();
-				foreach ($parts as $idx => $ip)
+				$tips = explode("\n", $tips);
+				foreach ($tips as $line)
 				{
-					if ($idx == 0)
-						continue;
-					$ips[$parts[0]][] = $ip;
+					$parts = explode(' ', $line);
+					$ips[$parts[0]] = array();
+					foreach ($parts as $idx => $ip)
+					{
+						if ($idx == 0)
+							continue;
+						$ips[$parts[0]][] = $ip;
+					}
+					//$servers[$id]['ips'] = $ips[$parts[0];
 				}
-				//$servers[$id]['ips'] = $ips[$parts[0];
 			}
 		}
 		$cmd = 'curl --connect-timeout 60 --max-time 240 -k -F action=serverlist -F servers="' . base64_encode(gzcompress(serialize($servers), 9)) . '"  ' . (isset($ips) ? ' -F ips="' . base64_encode(gzcompress(serialize($ips), 9)) . '" ' : '') . $curl_cmd . ' "' . $url . '" 2>/dev/null;';
