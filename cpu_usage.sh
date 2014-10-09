f88ec584dd8d5730690f39e1b918af0390420da2
Joe Huss <detain@interserver.net>
2014-10-09 13:40:46 +0000

    Just finished a script that gets CPU usage for every OpenVZ VPS every CPU/Core + VPS Total CPU Usage + Host Server CPU/Core Usage + Host Server CPU Total Usage, writen entirely in bash and extremely fast / efficient ,   parses /proc/vz/fairsched/<vzid>/cpu.proc.stat  files for info
    took *forever* to write this, and not a single person out there does this nearly as efficiently/fast  ,  ive looked through everyones source on gathering cpu usage,   and this way is way better  (either faster because it doesnt require entering the vps, more detailed including per core stats,  or more detailed including all vps's in one go) ... kinda odd the script in the end wasnt all that hard .. but seems nobodys written a parser for /proc/vz/fairsched/<vzid>/cpu.proc.stat directly yet , the closest is /proc/stat parsers in each vps (slower)

---
+++ cpu_usage.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+IFS="
+";
+if [ -e cpu_usage.last.sh ]; then
+	. cpu_usage.last.sh;
+else
+	declare -A cputotals;
+	declare -A cpuidles;
+fi;
+totalstring="declare -A cputotals=(";
+idlestring="declare -A cpuidles=(";
+for i in $(grep "^cpu" /proc/vz/fairsched/*/cpu.proc.stat | tr / " "  | tr : " " | awk '{ print $4 " " $6 " " $7 " " $8 " " $9 " " $10 " " $11 " " $12 " " $13 " " $14 }'); do
+	vzid="$(echo "$i" | awk '{ print $1 }')";
+	cpu="$(echo "$i" | awk '{ print $2 }')";
+	total="$(echo "$i" | awk '{ print $3 "+" $4 "+" $5 "+" $6 "+" $7 "+" $8 "+" $9}' |bc -l)";
+	idle="$(echo "$i" | awk '{ print $6 }')";
+	key="${vzid}_${cpu}";
+	totalstring="${totalstring}[${key}]=\"${total}\" ";
+	idlestring="${idlestring}[${key}]=\"${idle}\" ";
+	if [ ! -z "${cputotals[${key}]}" ]; then
+		cputotal=$((${total} - ${cputotals[${key}]}));
+		cpuidle=$((${idle} - ${cpuidles[${key}]}));
+		usage="$(echo "100 - (${cpuidle} / ${cputotal} * 100)" | bc -l)";
+		usage="$(echo "scale=2; ${usage}/1" | bc -l)";
+		echo "$vzid $cpu ${usage}%";
+	fi;
+done
+totalstring="${totalstring});\nexport cputotals;\n";
+idlestring="${idlestring});\nexport cpuidles;\n";
+echo -e "#\!/bin/bash\n${totalstring}${idlestring}" > cpu_usage.last.sh;
+chmod +x cpu_usage.last.sh;
+#DISP_SYS_RATE=$(echo "scale=${SCALE}; ${SYS_RATE}/1 "| bc);
