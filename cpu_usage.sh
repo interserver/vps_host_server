2b0fbf1796050f635708e5afe778e14220bd5790
Joe Huss <detain@interserver.net>
2014-10-10 09:45:41 +0000

    lots of work on my bash <4 suport

--- cpu_usage.sh
+++ cpu_usage.sh
@@ -48,8 +48,8 @@ for i in $(grep "^cpu" /proc/vz/fairsched/*/cpu.proc.stat | tr / " "  | tr : " "
 		idlestring="${idlestring}[${key}]=\"${idle}\" ";
 	fi;
 	if [ ! -z "$$tkey" ]; then
-		cputotal=$((${total} - $$tkey}));
-		cpuidle=$((${idle} - $$ikey}));
+		cputotal=$((${total} - $$tkey));
+		cpuidle=$((${idle} - $$ikey));
 		usage="$(echo "100 - (${cpuidle} / ${cputotal} * 100)" | bc -l)";
 		usage="$(echo "scale=2; ${usage}/1" | bc -l)";
 		if [ "${usage:0:1}" = "." ]; then
