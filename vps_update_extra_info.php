be86ce2dd22ae2218eb46676f0b85ec3d629f7c4
Joe Huss <detain@interserver.net>
2014-10-12 00:43:23 +0000

    updated the one script to use ethtool (and install it if its not) to get the current speed like 100mbit / gibt, etc..  and send it

--- vps_update_extra_info.php
+++ vps_update_extra_info.php
@@ -7,6 +7,9 @@
 	 */
 	function update_vps_extra_info()
 	{
+		// ensure ethtool is installed
+		`if ! which ethtool 2>/dev/null; then if [ -e /etc/redhat-release ]; then yum install -y ethtool; else apt-get install -y ethtool; fi; fi;`;
+		$speed = trim(`ethtool eth0 |grep Speed: | sed -e s#"^.* \([0-9]*\).*$"#"\1"#g`);
 		$flags = explode(' ', trim(`grep "^flags" /proc/cpuinfo | head -n 1 | cut -d: -f2-;`));
 		sort($flags);
 		$flagsnew = implode(' ', $flags);
@@ -14,6 +17,7 @@
 		unset($flagsnew);
 		$url = 'https://myvps2.interserver.net/vps_queue.php';
 		$servers = array();
+		$servers['speed'] = $speed;
 		$servers['cpu_flags'] = $flags;
 		$cmd = 'curl --connect-timeout 60 --max-time 240 -k -d action=vpsinfo_extra -d servers="' . urlencode(base64_encode(serialize($servers))) . '" "' . $url . '" 2>/dev/null;';
 		// echo "CMD: $cmd\n";
