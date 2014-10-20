e74b9fc944c695d0c753b982babd54b3d8989af6
Joe Huss <detain@interserver.net>
2014-10-20 14:22:43 +0000

    Additional features, output, etc.. to the cpu usage updater

--- cpu_usage_updater.sh
+++ cpu_usage_updater.sh
@@ -10,17 +10,36 @@ last=${start};
 spent=0;
 interval=10;
 maxtime=60;
+showts=0;
+[ $showts -eq 1 ] && echo -n "${new} ";
+echo "Getitng CPU usage every ${interval} seconds for the next ${maxtime} seconds";
 while [ ${spent} -lt ${maxtime} ]; do
+	new=$(date +%s);
+	first=$new;
+	prev=$new;
+	[ $showts -eq 1 ] && echo -n "${new} ";
+	echo -n "Grabbing";
+	cpu_usage="$(/root/cpaneldirect/cpu_usage.sh -serialize | sed s#"\""#"\&quot;"#g)";
+	new=$(date +%s);
+	lastspent=$((${new} - ${prev}));
+	prev=$new;
+	echo -n "(${lastspent}s),Sending";
 	curl --connect-timeout 60 --max-time 240 -k -D action=cpu_usage \
-		-D "cpu_usage=$(/root/cpaneldirect/cpu_usage.sh -serialize | sed s#"\""#"\&quot;"#g)" \
+		-D "cpu_usage=${cpu_usage}" \
 		"https://myvps2.interserver.net/vps_queue.php" 2>/dev/null;
 	new=$(date +%s);
-	lastspent=$((${new} - ${last}));
+	lastspent=$((${new} - ${prev}));
+	prev=$new;
+	echo -n "(${lastspent}s),Checking"
+	lastspent=$((${new} - ${first}));
 	spent=$((${new} - ${start}));
+	echo -n "(${lastspent}s)"
 	if [ ${lastspent} -lt ${interval} ]; then
 		speedy=$((${interval} - ${lastspent}));
+		echo -n ",Sleeping(${speedy}s)"
 		sleep ${speedy}s;
 		spent=$((${spent} + ${speedy}));
 	fi;
+	echo ",Overall(${spent}/${maxtime}s),Left($((${maxtime} - ${spent}))s)";
 	last=${new};
 done;
