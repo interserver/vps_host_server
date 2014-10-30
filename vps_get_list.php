3d132319892bee3cb4699f72e99757dc51a82a57
Joe Huss <detain@interserver.net>
2014-10-30 00:56:39 +0000

    some reworking of vps get list

--- vps_get_list.php
+++ vps_get_list.php
@@ -13,78 +13,6 @@ function get_vps_list()
 	$url = 'https://myvps2.interserver.net/vps_queue.php';
 	$curl_cmd = '';
 	$servers = array();
-	
-	//if (preg_match_all("/^[ ]*(?P<dev>[\w]+):(?P<inbytes>[\d]+)[ ]+(?P<inpackets>[\d]+)[ ]+(?P<inerrs>[\d]+)[ ]+(?P<indrop>[\d]+)[ ]+(?P<infifo>[\d]+)[ ]+(?P<inframe>[\d]+)[ ]+(?P<incompressed>[\d]+)[ ]+(?P<inmulticast>[\d]+)[ ]+(?P<outbytes>[\d]+)[ ]+(?P<outpackets>[\d]+)[ ]+(?P<outerrs>[\d]+)[ ]+(?P<outdrop>[\d]+)[ ]+(?P<outfifo>[\d]+)[ ]+(?P<outcolls>[\d]+)[ ]+(?P<outcarrier>[\d]+)[ ]+(?P<outcompressed>[\d]+)[ ]*$/im", file_get_contents('/proc/net/dev'), $matches))
-	if (preg_match_all("/^[ ]*([\w]+):([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]*$/im", file_get_contents('/proc/net/dev'), $matches))
-	{
-		$bw = array(time(), 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
-		foreach ($matches[1] as $idx => $dev)
-		{
-			if (substr($dev, 0, 3) == 'eth')
-			{
-				for ($x = 1; $x < 16; $x++)
-				{
-					$bw[$x] += $matches[$x+1][$idx]; 
-				}
-			}
-		}
-		$bw_usage = array(
-			'time' => $bw[0],
-			'bytes_in' => $bw[1],
-			'packets_in' => $bw[2],
-			'bytes_sec_in' => 0,
-			'packets_sec_in' => 0,
-			'bytes_out' => $bw[9],
-			'packets_out' => $bw[10],
-			'bytes_sec_out' => 0,
-			'packets_sec_out' => 0,
-			'bytes_total' => $bw[1] + $bw[9],
-			'packets_total' => $bw[2] + $bw[10],
-			'bytes_sec_total' => 0,
-			'packets_sec_total' => 0,
-		);
-		if (file_exists('/root/.bw_usage.last'))
-		{
-			$bw_last = unserialize(file_get_contents('/root/.bw_usage.last'));
-			$bw_usage_last = array(
-				'time' => $bw_last[0],
-				'bytes_in' => $bw_last[1],
-				'packets_in' => $bw_last[2],
-				'bytes_sec_in' => 0,
-				'packets_sec_in' => 0,
-				'bytes_out' => $bw_last[9],
-				'packets_out' => $bw_last[10],
-				'bytes_sec_out' => 0,
-				'packets_sec_out' => 0,
-				'bytes_total' => $bw_last[1] + $bw_last[9],
-				'packets_total' => $bw_last[2] + $bw_last[10],
-				'bytes_sec_total' => 0,
-				'packets_sec_total' => 0,
-			);
-			$time_diff = $bw[0] - $bw_last[0];
-			foreach(array('bytes', 'packets') as $stat)
-			{
-				foreach (array('in','out','total') as $dir)
-				{
-					$bw_usage[$stat . '_sec_' . $dir] = ($bw_usage[$stat . '_' . $dir] - $bw_usage_last[$stat . '_' . $dir]) / $time_diff; 
-				}
-			}
-		}
-		file_put_contents('/root/.bw_usage.last', serialize($bw));
-		$servers[0]['bw_usage'] = $bw_usage;
-	}
-	if (file_exists('/etc/redhat-release'))
-	{
-		preg_match('/^(?P<distro>[\w]+) release (?P<version>[\S]+)( .*)*$/i', file_get_contents('/etc/redhat-release'), $matches);
-	}
-	else
-	{
-		preg_match('/DISTRIB_ID=(?P<distro>[^<]+)<br>DISTRIB_RELEASE=(?P<version>[^<]+)<br>/i', str_replace("\n", '<br>', file_get_contents('/etc/lsb-release')), $matches);
-	}
-	$servers[0]['os_info'] = array(
-		'distro' => $matches['distro'],
-		'version' => $matches['version'],
-	);
 	if (!file_exists('/usr/sbin/vzctl'))
 	{
 		$out = trim(`export PATH="\$PATH:/bin:/usr/bin:/sbin:/usr/sbin";virsh list --all | grep -v -e "State\$" -e "------\$" -e "^\$" | awk "{ print \\\$2 \" \" \\\$3 }"`);
@@ -259,6 +187,77 @@ $cmd .= "./vncsnapshot -dieblank -compresslevel 0 -quality 70 -vncQuality 7 -jpe
 			}
 		}
 	}
+	//if (preg_match_all("/^[ ]*(?P<dev>[\w]+):(?P<inbytes>[\d]+)[ ]+(?P<inpackets>[\d]+)[ ]+(?P<inerrs>[\d]+)[ ]+(?P<indrop>[\d]+)[ ]+(?P<infifo>[\d]+)[ ]+(?P<inframe>[\d]+)[ ]+(?P<incompressed>[\d]+)[ ]+(?P<inmulticast>[\d]+)[ ]+(?P<outbytes>[\d]+)[ ]+(?P<outpackets>[\d]+)[ ]+(?P<outerrs>[\d]+)[ ]+(?P<outdrop>[\d]+)[ ]+(?P<outfifo>[\d]+)[ ]+(?P<outcolls>[\d]+)[ ]+(?P<outcarrier>[\d]+)[ ]+(?P<outcompressed>[\d]+)[ ]*$/im", file_get_contents('/proc/net/dev'), $matches))
+	if (preg_match_all("/^[ ]*([\w]+):([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]*$/im", file_get_contents('/proc/net/dev'), $matches))
+	{
+		$bw = array(time(), 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+		foreach ($matches[1] as $idx => $dev)
+		{
+			if (substr($dev, 0, 3) == 'eth')
+			{
+				for ($x = 1; $x < 16; $x++)
+				{
+					$bw[$x] += $matches[$x+1][$idx]; 
+				}
+			}
+		}
+		$bw_usage = array(
+			'time' => $bw[0],
+			'bytes_in' => $bw[1],
+			'packets_in' => $bw[2],
+			'bytes_sec_in' => 0,
+			'packets_sec_in' => 0,
+			'bytes_out' => $bw[9],
+			'packets_out' => $bw[10],
+			'bytes_sec_out' => 0,
+			'packets_sec_out' => 0,
+			'bytes_total' => $bw[1] + $bw[9],
+			'packets_total' => $bw[2] + $bw[10],
+			'bytes_sec_total' => 0,
+			'packets_sec_total' => 0,
+		);
+		if (file_exists('/root/.bw_usage.last'))
+		{
+			$bw_last = unserialize(file_get_contents('/root/.bw_usage.last'));
+			$bw_usage_last = array(
+				'time' => $bw_last[0],
+				'bytes_in' => $bw_last[1],
+				'packets_in' => $bw_last[2],
+				'bytes_sec_in' => 0,
+				'packets_sec_in' => 0,
+				'bytes_out' => $bw_last[9],
+				'packets_out' => $bw_last[10],
+				'bytes_sec_out' => 0,
+				'packets_sec_out' => 0,
+				'bytes_total' => $bw_last[1] + $bw_last[9],
+				'packets_total' => $bw_last[2] + $bw_last[10],
+				'bytes_sec_total' => 0,
+				'packets_sec_total' => 0,
+			);
+			$time_diff = $bw[0] - $bw_last[0];
+			foreach(array('bytes', 'packets') as $stat)
+			{
+				foreach (array('in','out','total') as $dir)
+				{
+					$bw_usage[$stat . '_sec_' . $dir] = ($bw_usage[$stat . '_' . $dir] - $bw_usage_last[$stat . '_' . $dir]) / $time_diff; 
+				}
+			}
+		}
+		file_put_contents('/root/.bw_usage.last', serialize($bw));
+		$servers[0]['bw_usage'] = $bw_usage;
+	}
+	if (file_exists('/etc/redhat-release'))
+	{
+		preg_match('/^(?P<distro>[\w]+) release (?P<version>[\S]+)( .*)*$/i', file_get_contents('/etc/redhat-release'), $matches);
+	}
+	else
+	{
+		preg_match('/DISTRIB_ID=(?P<distro>[^<]+)<br>DISTRIB_RELEASE=(?P<version>[^<]+)<br>/i', str_replace("\n", '<br>', file_get_contents('/etc/lsb-release')), $matches);
+	}
+	$servers[0]['os_info'] = array(
+		'distro' => $matches['distro'],
+		'version' => $matches['version'],
+	);
 	$cmd = 'curl --connect-timeout 60 --max-time 240 -k -F action=serverlist -F servers="' . base64_encode(gzcompress(serialize($servers), 9)) . '"  ' 
 	. (isset($ips) ? ' -F ips="' . base64_encode(gzcompress(serialize($ips), 9)) . '" ' : '') 
 //	. ($cpu_data != '' ? ' -F cpu_usage="' . base64_encode(gzcompress($cpu_data, 9)) . '" ' : '')
