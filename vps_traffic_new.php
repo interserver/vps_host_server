879eb0237adb9567fe91d6600bbc013380f2c7de
Joe Huss <detain@interserver.net>
2014-11-28 22:36:31 +0000

    updates to the vps traffic coee

--- vps_traffic_new.php
+++ vps_traffic_new.php
@@ -81,53 +81,50 @@ function get_vps_iptables_traffic($ips)
 			//echo "Got    VNet:$vnet   IN:$in    OUT:$out\n";
 			$vnets[$vnet] = array('in' => $in, 'out' => $out);
 		}
-		$cmd = 'grep -i fe /sys/devices/virtual/net/vnet*/address | sed s#"/sys/devices/virtual/net/\([^/]*\)/address:fe:\(.*\)$"#"\1 52:\2"#g';
+		$cmd = 'grep -i fe /sys/devices/virtual/net/vnet*/address 2>/dev/null| sed s#"/sys/devices/virtual/net/\([^/]*\)/address:fe:\(.*\)$"#"\1 52:\2"#g';
 		$vnetmacs = explode("\n", trim(`$cmd`));
-		$macs = array();
-		foreach ($vnetmacs as $line)
+		if (sizeof($vnetmacs) > 0)
 		{
-			list($vnet, $mac) = explode(' ', $line);
-			//echo "Got  VNet:$vnet   Mac:$mac\n";
-			$vnets[$vnet]['mac'] = $mac;
-			$macs[$mac] = $vnet;
-		}
-		$cmd = 'if [ -e /etc/dhcp/dhcpd.vps ]; then cat /etc/dhcp/dhcpd.vps; else cat /etc/dhcpd.vps; fi | grep ethernet | sed s#"^host \([a-z0-9\.]*\) { hardware ethernet \([^;]*\); fixed-address \([0-9\.]*\);}$"#"\2 \1 \3"#g';
-		$macvps = explode("\n", trim(`$cmd`));
-		$vpss = array();
-		foreach ($macvps as $line)
-		{
-			list($mac, $vps, $ip) = explode(' ', $line);
-			//echo "Got  Mac:$mac   VPS:$vps   IP:$ip\n";
-			if (isset($macs[$mac]) && isset($vnets[$macs[$mac]]))
+			$macs = array();
+			foreach ($vnetmacs as $line)
 			{
-				$vpss[$vps] = $vnets[$macs[$mac]];
-				$vpss[$vps]['ip'] = $ip;
-				if (isset($last))
+				list($vnet, $mac) = explode(' ', $line);
+				//echo "Got  VNet:$vnet   Mac:$mac\n";
+				$vnets[$vnet]['mac'] = $mac;
+				$macs[$mac] = $vnet;
+			}
+			$cmd = 'if [ -e /etc/dhcp/dhcpd.vps ]; then cat /etc/dhcp/dhcpd.vps; else cat /etc/dhcpd.vps; fi | grep ethernet | sed s#"^host \([a-z0-9\.]*\) { hardware ethernet \([^;]*\); fixed-address \([0-9\.]*\);}$"#"\2 \1 \3"#g';
+			$macvps = explode("\n", trim(`$cmd`));
+			$vpss = array();
+			foreach ($macvps as $line)
+			{
+				list($mac, $vps, $ip) = explode(' ', $line);
+				//echo "Got  Mac:$mac   VPS:$vps   IP:$ip\n";
+				if (isset($macs[$mac]) && isset($vnets[$macs[$mac]]))
 				{
-					$in_new = bcsub($vpss[$vps]['in'], $last[$vps]['in'], 0);
-					$out_new = bcsub($vpss[$vps]['out'], $last[$vps]['out'], 0);
-				}
-				else
-				{
-					$in_new = $vpss[$vps]['in'];
-					$out_new = $vpss[$vps]['out'];
-				} 
-				if ($in_new > 0 || $out_new > 0)
-				{
-					$totals[$ip] = array('in' => $in_new, 'out' => $out_new);
+					$vpss[$vps] = $vnets[$macs[$mac]];
+					$vpss[$vps]['ip'] = $ip;
+					if (isset($last))
+					{
+						$in_new = bcsub($vpss[$vps]['in'], $last[$vps]['in'], 0);
+						$out_new = bcsub($vpss[$vps]['out'], $last[$vps]['out'], 0);
+					}
+					else
+					{
+						$in_new = $vpss[$vps]['in'];
+						$out_new = $vpss[$vps]['out'];
+					} 
+					if ($in_new > 0 || $out_new > 0)
+					{
+						$totals[$ip] = array('in' => $in_new, 'out' => $out_new);
+					}
 				}
 			}
+			if (sizeof($totals) > 0)
+			{
+				file_put_contents('/root/.traffic.last', serialize($vpss));
+			}
 		}
-		if (sizeof($totals) > 0)
-		{
-			file_put_contents('/root/.traffic.last', serialize($vpss));
-		}
-/*
-Gives us an array like this:
-[windows42488] => Array([in] => 3610790, [out] => 122200856, [mac] => 52:54:00:0a:89:74, [ip] => 162.250.121.126 )
-[windows42498] => Array([in] => 2550889, [out] => 120552888, [mac] => 52:54:00:cc:83:eb, [ip] => 162.250.120.242 )
-[windows42504] => Array([in] => 596300,  [out] => 26122451,  [mac] => 52:54:00:d5:00:7c, [ip] => 162.250.120.243 )
-*/		
 	}
 	else
 	{
