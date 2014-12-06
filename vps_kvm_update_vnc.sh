c8d93a27214a18d3b31c2b17d0936bdf8ebf59ff
Joe Huss <detain@interserver.net>
2014-12-06 19:31:32 +0000

    updates

--- vps_kvm_update_vnc.sh
+++ vps_kvm_update_vnc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin:/usr/X11R6/bin:/root/bin"
-myip=$(/sbin/ifconfig eth0 | grep "inet addr" | cut -d: -f2| cut -d" " -f1)
+myip="$(ifconfig $(ip route list | grep "^default" | sed s#"^default.*dev "#""#g | cut -d" " -f1)  |grep inet |grep -v inet6 | awk '{ print $2 }' | cut -d: -f2)"
 name=$1
 if [ $# -ne 1 ]; then
  echo "Open VNC To IP"
