fa89032fb84f8aa613feffa42b7846758dea6f32
Joe Huss <detain@interserver.net>
2015-02-01 01:16:48 +0000

    updates

--- savevps.sh
+++ savevps.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-  if [ "\$1" = "" ]; then 
-    guess="\$(top -c -b -n 1 | grep "[[:digit:]] qemu" | sed "s/^.* -name \([^ ]*\) .*\$/\1/g" | head -n 1)";
-    read -e -i "\$guess" -p "VPS Name: " vps;
+  if [ "$1" = "" ]; then 
+    guess="$(top -c -b -n 1 | grep "[[:digit:]] qemu" | sed "s/^.* -name \([^ ]*\) .*$/\1/g" | head -n 1)";
+    read -e -i "$guess" -p "VPS Name: " vps;
   else 
-    vps="\$1"; 
+    vps="$1"; 
   fi; 
-  if [ "\$(echo "\$vps" |grep "^[a-z]")" = "" ]; then   
+  if [ "$(echo "$vps" |grep "^[a-z]")" = "" ]; then   
     if [ -e /win ]; then
-      vps="windows\$vps";
+      vps="windows$vps";
     else
-      vps="linux\$vps";
+      vps="linux$vps";
     fi;
   fi;
-  virsh save \$vps /var/lib/libvirt/autosuspend/\$vps.dump;
+  virsh save $vps /var/lib/libvirt/autosuspend/$vps.dump;
 
