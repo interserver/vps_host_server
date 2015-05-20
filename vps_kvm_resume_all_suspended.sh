73183ae338c7b916faa1daf81c4bc2c5867c1cf9
Joe Huss <detain@interserver.net>
2015-05-20 04:46:29 +0000

    Updates to the resume all suspended vops scripts

--- vps_kvm_resume_all_suspended.sh
+++ vps_kvm_resume_all_suspended.sh
@@ -3,7 +3,8 @@ suspended="$(ls /var/lib/libvirt/autosuspend/ 2>/dev/null | sed s#"\.dump$"#""#g
 if [ "$suspended" != "" ]; then
 	for vps in $suspended; do
 		virsh destroy $vps; 
-done; 
-/etc/init.d/libvirt-suspendonreboot start
-/root/cpaneldirect/run_buildebtables.sh
-/root/cpaneldirect/vps_refresh_all_vnc.sh
+	done; 
+	/etc/init.d/libvirt-suspendonreboot start;
+fi;
+/root/cpaneldirect/run_buildebtables.sh;
+/root/cpaneldirect/vps_refresh_all_vnc.sh;
