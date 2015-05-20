70e110e2379ac4ca0b465901359e43f55fd6a194
Joe Huss <detain@interserver.net>
2015-05-20 04:44:50 +0000

    Added a script to resume all vpses on a rfreshly booted ubuntu system

---
+++ vps_kvm_resume_all_suspended.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+suspended="$(ls /var/lib/libvirt/autosuspend/ 2>/dev/null | xargs -n 1 basename | sed s#"\.dump$"#""#g)";
+if [ "$suspended" != "" ]; then
+	for vps in $suspended; do
+		virsh destroy $vps; 
+done; 
+/etc/init.d/libvirt-suspendonreboot start
+/root/cpaneldirect/run_buildebtables.sh
+/root/cpaneldirect/vps_refresh_all_vnc.sh
