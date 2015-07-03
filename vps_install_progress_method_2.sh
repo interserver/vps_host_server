5e70147ac661bb5ce1a287b4414b70f9f338a742
Joe Huss <detain@interserver.net>
2015-07-03 19:36:35 +0000

    updates to the install progress scrpit

--- vps_install_progress_method_2.sh
+++ vps_install_progress_method_2.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-cat /proc/$(pidof dd)/fd/2 > dd.out 2>/dev/null &
-while [ "$(grep "^[[:digit:]]*.*bytes.*copied.*/s$" dd.out)" = "" ]; do
-	killall -USR1 dd
-	sleep 0.1s;
-done
-grep "^[[:digit:]]*.*bytes.*copied.*/s$" dd.out
-killall cat >/dev/null 2>/dev/null
-rm -f dd.out;
+{
+	cat /proc/$(pidof dd)/fd/2 > dd.out 2>/dev/null &
+	while [ "$(grep "^[[:digit:]]*.*bytes.*copied.*/s$" dd.out 2>/dev/null)" = "" ]; do
+		killall -USR1 dd
+		sleep 0.1s;
+	done
+	grep "^[[:digit:]]*.*bytes.*copied.*/s$" dd.out
+	killall cat >/dev/null 2>/dev/null
+	rm -f dd.out;
+} 2>&1 | grep -v Terminated
