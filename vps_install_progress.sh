6e86d4c7ffb59c5607267cb182474866ca07ff81
Joe Huss <detain@interserver.net>
2015-07-16 14:49:46 +0000

    updated the vps_install_progress.sh script to also tell what vps is installing/reinstalling

--- vps_install_progress.sh
+++ vps_install_progress.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 base="$(readlink -f "$(dirname "$0")")";
-echo "$($base/vps_install_progress_method_2.sh) ($($base/vps_install_progress_method_1.sh)%)"
+echo "$($base/vps_install_progress_method_2.sh) ($($base/vps_install_progress_method_1.sh)%) on $(ps x |grep "/bin/bash /root/cpaneldirect/vps_kvm_create.sh" |grep -v " grep " | awk '{ print $7 }')"
