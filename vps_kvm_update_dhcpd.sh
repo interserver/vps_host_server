885b1e385b7f5e9e672298e5f65c45fc32754705
Joe Huss <detain@interserver.net>
2015-08-24 21:53:11 +0000

    updated the vps kvm update dhcpd script

--- vps_kvm_update_dhcpd.sh
+++ vps_kvm_update_dhcpd.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-export PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin:/usr/X11R6/bin:/root/bin"
-name=$1
-size=$2
+export PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin:/usr/X11R6/bin:/root/bin";
 IFS="
-"
+";
 if [ -e /etc/dhcp/dhcpd.vps ]; then
-	DHCPVPS=/etc/dhcp/dhcpd.vps
+	DHCPVPS=/etc/dhcp/dhcpd.vps;
+	/bin/rm -f /etc/dhcpd.vps;
+	ln -s /etc/dhcp/dhcpd.vps /etc/dhcpd.vps;
 else
-	DHCPVPS=/etc/dhcpd.vps
-fi
+	DHCPVPS=/etc/dhcpd.vps;
+fi;
 for user in $(/usr/bin/virsh list | grep running | awk '{print $2}'); do
 	mac=`/usr/bin/virsh dumpxml $user | grep "mac" | grep address | grep : | cut -d\' -f2`;
-	cat ${DHCPVPS} | sed s#"host $user { hardware ethernet .*; fix"#"host $user { hardware ethernet $mac; fix"#g > ${DHCPVPS}.new
-	cat ${DHCPVPS}.new > ${DHCPVPS}
-	rm -f ${DHCPVPS}.new ${DHCPVPS}
-done
- if [ ! -e /etc/init.d/dhcpd ] && [ -e /etc/init.d/isc-dhcp-server ]; then
-  /etc/init.d/isc-dhcp-server restart
- elif [ -e /etc/init.d/dhcpd ]; then
-  /etc/init.d/dhcpd restart
- else
-  service dhcpd restart
- fi
+	cat ${DHCPVPS} | sed s#"host $user { hardware ethernet .*; fix"#"host $user { hardware ethernet $mac; fix"#g > ${DHCPVPS}.new;
+	cat ${DHCPVPS}.new > ${DHCPVPS};
+	rm -f ${DHCPVPS}.new ${DHCPVPS};
+done;
+if [ ! -e /etc/init.d/dhcpd ] && [ -e /etc/init.d/isc-dhcp-server ]; then
+	/etc/init.d/isc-dhcp-server restart;
+elif [ -e /etc/init.d/dhcpd ]; then
+	/etc/init.d/dhcpd restart;
+else
+	service dhcpd restart;
+fi;
