a996541223052ab2e3f2f08ead3dc6dde932dd33
Joe Huss <detain@interserver.net>
2015-08-26 17:24:37 +0000

    fix for the rebuild scrip

--- vps_kvm_update_dhcpd.sh
+++ vps_kvm_update_dhcpd.sh
@@ -14,7 +14,7 @@ for user in $(/usr/bin/virsh list | grep running | awk '{print $2}'); do
 	mac=`/usr/bin/virsh dumpxml $user | grep "mac" | grep address | grep : | cut -d\' -f2`;
 	cat ${DHCPVPS} | sed s#"host $user { hardware ethernet .*; fix"#"host $user { hardware ethernet $mac; fix"#g > ${DHCPVPS}.new;
 	cat ${DHCPVPS}.new > ${DHCPVPS};
-	rm -f ${DHCPVPS}.new ${DHCPVPS};
+	rm -f ${DHCPVPS}.new;
 done;
 if [ ! -e /etc/init.d/dhcpd ] && [ -e /etc/init.d/isc-dhcp-server ]; then
 	/etc/init.d/isc-dhcp-server restart;
