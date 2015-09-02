e29754acadc2c3be1ff3df71fce35431828e3e67
Joe Huss <detain@interserver.net>
2015-09-02 16:02:55 +0000

    updates to the vps get list script dealing with some vrious ethernet device names

--- vps_get_list.php
+++ vps_get_list.php
@@ -265,7 +265,20 @@ $cmd .= "./vncsnapshot -dieblank -compresslevel 0 -quality 70 -vncQuality 7 -jpe
 	// ensure ethtool is installed
 	`if ! which ethtool 2>/dev/null; then if [ -e /etc/redhat-release ]; then yum install -y ethtool; else apt-get install -y ethtool; fi; fi;`;
 	//$speed = trim(`ethtool eth0 |grep Speed: | sed -e s#"^.* \([0-9]*\).*$"#"\1"#g`);
-	$cmd = 'ethtool eth0 |grep Speed: | sed -e s#"^.* \([0-9]*\).*$"#"\1"#g';
+	if (in_array($_SERVER['HOSTNAME'], array("kvm1.trouble-free.net", "kvm2.interserver.net", "kvm50.interserver.net")))
+		$eth = 'eth1';
+	elseif (file_exists('/etc/debian_version'))
+	{
+		if (file_exists('/sys/class/net/p2p1'))
+			$eth = 'p2p1';
+		elseif (file_exists('/sys/class/net/em1'))
+			$eth = 'em1';
+		else
+			$eth = 'eth0';
+	}
+	else
+		$eth = 'eth0';
+	$cmd = 'ethtool '.$eth.' |grep Speed: | sed -e s#"^.* \([0-9]*\).*$"#"\1"#g';
 	//echo "Running {$cmd}\n";
 	$speed = trim(`{$cmd}`);
 	$cpuinfo = explode("\n", file_get_contents('/proc/cpuinfo'));
