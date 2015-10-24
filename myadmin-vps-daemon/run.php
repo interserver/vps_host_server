ac84338887d73641071b2536e41fc2cdae60659e
Joe Huss <detain@interserver.net>
2015-10-24 16:21:27 +0000

    Started work on another daemon to run on the vps host servers, i might wind up going with this one ..

---
+++ myadmin-vps-daemon/run.php
@@ -0,0 +1,9 @@
+#!/usr/bin/php
+<?php
+require_once 'config.php';
+require_once 'error_handlers.php';
+
+use MyAdmin\Daemon;
+
+// The run() method will start the daemon event loop.
+MyAdmin\Daemon\Poller::getInstance()->run();
