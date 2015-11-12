9eb9189faa744e2aa00b2703105945555cc30e6a
Joe Huss <detain@interserver.net>
2015-11-12 20:17:04 +0000

    updates

--- nagios-plugin-check_raid_prerequisites.sh
+++ nagios-plugin-check_raid_prerequisites.sh
@@ -3,6 +3,9 @@ if [ -e /etc/redhat-release ]; then
   distro=centos; 
   version=$(cat /etc/redhat-release  | sed s#"^.* \([0-9]\)\..*$"#"\1"#g); 
   yum install -y perl-CPAN yaml-cpp yaml-cpp-devel perl-YAML libyaml libyaml-devel perl-Test-CPAN-Meta-YAML perl-Test-YAML-Meta expect libserf-devel db4-devel gnome-keyring-devel >&2;
+  if [ ! -e /usr/bin/scons ]; then
+     rpm --install http://prdownloads.sourceforge.net/scons/scons-2.4.1-1.noarch.rpm
+  fi;
 elif [ -e /etc/apt ]; then 
   . /etc/lsb-release; 
   distro=ubuntu; 
