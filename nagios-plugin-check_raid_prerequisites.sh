821b79ee0f48c81a334b966986c644b552d950a6
Joe Huss <detain@interserver.net>
2015-11-12 06:50:44 +0000

    updates

--- nagios-plugin-check_raid_prerequisites.sh
+++ nagios-plugin-check_raid_prerequisites.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for i in  Monitoring::Plugin Module::Pluggable ExtUtils::MakeMaker ExtUtils::MakeMaker::CPANfile ; do 
-	PERL_MM_USE_DEFAULT=1 cpan install $i; 
+	PERL_MM_USE_DEFAULT=1 cpan $i; 
 done
