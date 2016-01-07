dfaaefd675f9058b0c5fac0cf0609bb0f9e6d35c
Joe Huss <detain@interserver.net>
2016-01-07 19:14:44 +0000

    Updated the vps_install_progress.sh script to now also calculate and display an estimate time till finished in seconds.

--- vps_install_progress.sh
+++ vps_install_progress.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 base="$(readlink -f "$(dirname "$0")")";
 echo "$($base/vps_install_progress_method_2.sh) ($($base/vps_install_progress_method_1.sh)%) on $(ps x |grep "/bin/bash /root/cpaneldirect/vps_kvm_create.sh" |grep -v " grep " | awk '{ print $7 }')"
+pct=$($base/vps_install_progress_method_1.sh);
+meth2="$($base/vps_install_progress_method_2.sh)";
+time="$(echo "${meth2}" | cut -d"," -f2 | tr "." " " | awk '{ print $1 }')";
+echo "Time $time  Pct $pct"
+eta="$(echo "((${time} / ${pct}) * 100) - ${time}" | bc -l | sed s#"\..*"#""#g)";
+echo "${meth2}  (${pct}%) on $(ps x |grep "/bin/bash /root/cpaneldirect/vps_kvm_create.sh" |grep -v " grep " | awk '{ print $7 }') eta ${eta} seconds"
