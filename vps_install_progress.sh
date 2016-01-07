5ea89f633ee407d130d898a3b4b1c3990d114ebb
Joe Huss <detain@interserver.net>
2016-01-07 19:18:58 +0000

    updates to install progress

--- vps_install_progress.sh
+++ vps_install_progress.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 base="$(readlink -f "$(dirname "$0")")";
-echo "$($base/vps_install_progress_method_2.sh) ($($base/vps_install_progress_method_1.sh)%) on $(ps x |grep "/bin/bash /root/cpaneldirect/vps_kvm_create.sh" |grep -v " grep " | awk '{ print $7 }')"
 pct=$($base/vps_install_progress_method_1.sh);
 meth2="$($base/vps_install_progress_method_2.sh)";
 time="$(echo "${meth2}" | cut -d"," -f2 | tr "." " " | awk '{ print $1 }')";
