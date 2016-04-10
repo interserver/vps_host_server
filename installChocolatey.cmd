26bf227974fe76f4984c218a4184e9a91e42dcfa
Joe Huss <detain@interserver.net>
2016-04-10 09:37:17 +0000

    removing file not needed her

--- installChocolatey.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-
-SET DIR=%~dp0%
-
-::download install.ps1
-%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "((new-object net.webclient).DownloadFile('https://chocolatey.org/install.ps1','install.ps1'))"
-::run installer
-%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '%DIR%install.ps1' %*"
-
