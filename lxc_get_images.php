3c9b35905f333cfd858fe81c5aec73c026e02030
Joe Huss <detain@interserver.net>
2016-05-16 07:51:12 +0000

    added a sceript oi wwrote ot parse out the lxc images

---
+++ lxc_get_images.php
@@ -0,0 +1,17 @@
+<?php
+echo "Building LXC Image List\n";
+$out = `lxc image list images:;`;
+preg_match_all('/^\|\s*(?P<alias>\S+)\s.*\|\s*(?P<fingerprint>[a-z0-9]*)\s*\|\s*(?P<public>\S+)\s*\|\s*(?P<description>.*)\s*\|\s*(?P<arch>i686|x86_64)\s*\|\s*(?P<size>\S+)\s*\|\s*(?P<upload_date>.*)\s*\|$/mU', $out, $matches);
+foreach ($matches[0] as $idx => $match)
+	$images[$idx] = array(
+		'alias' => $matches['alias'][$idx],
+		'fingerprint' => $matches['fingerprint'][$idx],
+		'public' => $matches['public'][$idx],
+		'description' => $matches['description'][$idx],
+		'arch' => $matches['arch'][$idx],
+		'size' => $matches['size'][$idx],
+		'upload_date' => $matches['upload_date'][$idx],
+	);	
+echo json_encode($images, JSON_PRETTY_PRINT) . "\n";
+//print_r($images);
+?>
