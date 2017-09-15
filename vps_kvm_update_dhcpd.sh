b2c296a32ab5373e0af4a17f6037ab4236fd3113
Joe Huss <detain@interserver.net>
2017-09-15 07:09:30 +0000

    updates to vps kvm update dhcpd script removing some old code and adding a check to make sure it created the vps.mainips file before trying to parse it.

--- vps_kvm_update_dhcpd.sh
+++ vps_kvm_update_dhcpd.sh
@@ -1,36 +1,35 @@
 #!/bin/bash
 export PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin:/usr/X11R6/bin:/root/bin";
-IFS="
-";
-if [ "$(crontab -l|grep qs_cron)" != "" ]; then 
+if [ "$(crontab -l|grep qs_cron)" != "" ]; then
 	url="https://myquickserver2.interserver.net/qs_queue.php"
 else
 	url="https://myvps2.interserver.net/vps_queue.php";
 fi
-if [ -e /etc/dhcp/dhcpd.vps ]; then
-	DHCPVPS=/etc/dhcp/dhcpd.vps;
-	/bin/rm -f /etc/dhcpd.vps;
-	ln -s /etc/dhcp/dhcpd.vps /etc/dhcpd.vps;
-else
-	DHCPVPS=/etc/dhcpd.vps;
-fi;
-if [ -e ${DHCPVPS} ]; then
-	/bin/mv -f ${DHCPVPS} ${DHCPVPS}.backup;
-fi;
 curl --connect-timeout 300 --max-time 600 -k -d action=getvpsmainips "$url" 2>/dev/null | sh;
-for user in $(/usr/bin/virsh list --all | grep running | awk '{print $2}' |grep -v "^guestfs-"); do
-	mac=`/usr/bin/virsh dumpxml $user | grep "mac" | grep address | grep : | cut -d\' -f2`;
-	id="$(echo "$user" | sed s#"[[:alpha:]]"#""#g)"
-	ip="$(grep "^$id:" /root/cpaneldirect/vps.mainips | cut -d: -f2-)";
-	echo "host $user { hardware ethernet $mac; fixed-address $ip;}" >> ${DHCPVPS};
-	#cat ${DHCPVPS} | sed s#"host $user { hardware ethernet .*; fix"#"host $user { hardware ethernet $mac; fix"#g > ${DHCPVPS}.new;
-	#cat ${DHCPVPS}.new > ${DHCPVPS};
-	#rm -f ${DHCPVPS}.new;
-done;
-if [ ! -e /etc/init.d/dhcpd ] && [ -e /etc/init.d/isc-dhcp-server ]; then
-	/etc/init.d/isc-dhcp-server restart;
-elif [ -e /etc/init.d/dhcpd ]; then
-	/etc/init.d/dhcpd restart;
-else
-	service dhcpd restart;
+if [ -e /root/cpaneldirect/vps.mainips ]; then
+	IFS="
+";
+	if [ -e /etc/dhcp/dhcpd.vps ]; then
+		DHCPVPS=/etc/dhcp/dhcpd.vps;
+		/bin/rm -f /etc/dhcpd.vps;
+		ln -s /etc/dhcp/dhcpd.vps /etc/dhcpd.vps;
+	else
+		DHCPVPS=/etc/dhcpd.vps;
+	fi;
+	if [ -e ${DHCPVPS} ]; then
+		/bin/mv -f ${DHCPVPS} ${DHCPVPS}.backup;
+	fi;
+	for user in $(virsh list | grep running | awk '{print $2}' |grep -v "^guestfs-"); do
+		mac=`virsh dumpxml $user | grep "mac" | grep address | grep : | cut -d\' -f2`;
+		id="$(echo "$user" | sed s#"[[:alpha:]]"#""#g)"
+		ip="$(grep "^$id:" /root/cpaneldirect/vps.mainips | cut -d: -f2-)";
+		echo "host $user { hardware ethernet $mac; fixed-address $ip;}" >> ${DHCPVPS};
+	done;
+	if [ ! -e /etc/init.d/dhcpd ] && [ -e /etc/init.d/isc-dhcp-server ]; then
+		/etc/init.d/isc-dhcp-server restart;
+	elif [ -e /etc/init.d/dhcpd ]; then
+		/etc/init.d/dhcpd restart;
+	else
+		service dhcpd restart;
+	fi;
 fi;
