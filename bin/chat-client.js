83bfc397c9dda6c42cd0a6cb9d917ee43dfa3459
Joe Huss <detain@interserver.net>
2017-09-16 07:15:52 +0000

    updates adding intial chat / ws demo code in ratchet

---
+++ bin/chat-client.js
@@ -0,0 +1,8 @@
+var conn = new WebSocket('ws://localhost:8080');
+conn.onopen = function(e) {
+    console.log("Connection established!");
+};
+
+conn.onmessage = function(e) {
+    console.log(e.data);
+};
