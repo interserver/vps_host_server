abc50366502a723d2b3106e330ac974cc91a7c97
Joe Huss <detain@interserver.net>
2018-01-24 01:57:32 +0000

    movign files

--- workerman/Applications/Chat/start_register.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use \Workerman\Worker;
-use \GatewayWorker\Register;
-
-$register = new Register('text://0.0.0.0:1236'); // register service must be a text protocol
-
-if(!defined('GLOBAL_START')) // If it is not started in the root directory, run the runAll method
-	Worker::runAll();
