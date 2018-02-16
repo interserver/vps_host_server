89e2e103f53a4e330e94d103140ffc9052637f25
Joe Huss <detain@interserver.net>
2018-02-16 08:02:51 +0000

    moving some stuff around

--- workerman/src/bootstrap.php
+++ workerman/src/bootstrap.php
@@ -2,5 +2,5 @@
 global $composer, $settings;
 
 $composer = include __DIR__.'/vendor/autoload.php';
-$settings = include __DIR__.'/src/settings.php';
+$settings = include __DIR__.'/src/Config/settings.php';
 include_once __DIR__.'/src/functions.php';
