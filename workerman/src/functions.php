f0c37ef2264311e1e4df81fe601658ab6355f8a4
Joe Huss <detain@interserver.net>
2018-02-16 04:53:48 +0000

    cleaning up this code a bit

--- workerman/src/functions.php
+++ workerman/src/functions.php
@@ -101,36 +101,25 @@ function vps_get_list($params) {
 					'hostname' => $name,
 					'kmemsize' => $xml['domain']['memory'],
 				);
-				if (isset($xml['domain']['devices']['interface']))
-				{
+				if (isset($xml['domain']['devices']['interface'])) {
 					if (isset($xml['domain']['devices']['interface']['mac_attr']))
 						$server['mac'] = $xml['domain']['devices']['interface']['mac_attr']['address'];
 					elseif (isset($xml['domain']['devices']['interface'][0]['mac_attr']))
 						$server['mac'] = $xml['domain']['devices']['interface'][0]['mac_attr']['address'];
 				}
-				if (isset($xml['domain']['devices']['graphics_attr']))
-				{
+				if (isset($xml['domain']['devices']['graphics_attr'])) {
 					$server['vnc'] = $xml['domain']['devices']['graphics_attr']['port'];
 				}
-				if ($status == 'running')
-				{
-
-
+				if ($status == 'running') {
 					$disk = trim(`/root/cpaneldirect/vps_kvm_disk_usage.sh $name`);
-					if ($disk != '')
-					{
+					if ($disk != '') {
 						$dparts = explode(':', $disk);
 						$server['diskused'] = $dparts[2];
 						$server['diskmax'] = $dparts[1];
 					}
-
-
-
-					if (isset($xml['domain']['devices']['graphics_attr']))
-					{
+					if (isset($xml['domain']['devices']['graphics_attr'])) {
 						$port = (integer)$xml['domain']['devices']['graphics_attr']['port'];
-						if ($port >= 5900)
-						{
+						if ($port >= 5900) {
 // vncsnapshot Encodings: raw copyrect tight hextile zlib corre rre zrle
 $cmd .= "if [ -e /usr/bin/timeout ]; then
 	timeout 30s ./vncsnapshot -dieblank -compresslevel 0 -quality 70 -vncQuality 7 -jpeg -fps 5 -count 1 -quiet -encodings raw :\$(($port - 5900)) shot_{$port}.jpg >/dev/null 2>&1;
@@ -151,10 +140,8 @@ fi;\n";
 				$servers[$veid] = $server;
 			}
 		}
-		if ($cpu_usage = @unserialize(`bash /root/cpaneldirect/cpu_usage.sh -serialize`))
-		{
-			foreach ($cpu_usage as $id => $cpu_data)
-			{
+		if ($cpu_usage = @unserialize(`bash /root/cpaneldirect/cpu_usage.sh -serialize`)) {
+			foreach ($cpu_usage as $id => $cpu_data) {
 				//$servers[$id]['cpu_usage'] = serialize($cpu_data);
 				$servers[$id]['cpu_usage'] = $cpu_data;
 			}
@@ -176,8 +163,7 @@ fi;\n";
 			preg_match_all('/\s+(?P<ctid>[^\s]+)\s+(?P<numproc>[^\s]+)\s+(?P<status>[^\s]+)\s+(?P<ip>[^\s]+)\s+(?P<hostname>[^\s]+)\s+(?P<vswap>[^\s]+)\s+(?P<layout>[^\s]+)\s+(?P<kmemsize>[^\s]+)\s+(?P<kmemsize_f>[^\s]+)\s+(?P<lockedpages>[^\s]+)\s+(?P<lockedpages_f>[^\s]+)\s+(?P<privvmpages>[^\s]+)\s+(?P<privvmpages_f>[^\s]+)\s+(?P<shmpages>[^\s]+)\s+(?P<shmpages_f>[^\s]+)\s+(?P<numproc_f>[^\s]+)\s+(?P<physpages>[^\s]+)\s+(?P<physpages_f>[^\s]+)\s+(?P<vmguarpages>[^\s]+)\s+(?P<vmguarpages_f>[^\s]+)\s+(?P<oomguarpages>[^\s]+)\s+(?P<oomguarpages_f>[^\s]+)\s+(?P<numtcpsock>[^\s]+)\s+(?P<numtcpsock_f>[^\s]+)\s+(?P<numflock>[^\s]+)\s+(?P<numflock_f>[^\s]+)\s+(?P<numpty>[^\s]+)\s+(?P<numpty_f>[^\s]+)\s+(?P<numsiginfo>[^\s]+)\s+(?P<numsiginfo_f>[^\s]+)\s+(?P<tcpsndbuf>[^\s]+)\s+(?P<tcpsndbuf_f>[^\s]+)\s+(?P<tcprcvbuf>[^\s]+)\s+(?P<tcprcvbuf_f>[^\s]+)\s+(?P<othersockbuf>[^\s]+)\s+(?P<othersockbuf_f>[^\s]+)\s+(?P<dgramrcvbuf>[^\s]+)\s+(?P<dgramrcvbuf_f>[^\s]+)\s+(?P<numothersock>[^\s]+)\s+(?P<numothersock_f>[^\s]+)\s+(?P<dcachesize>[^\s]+)\s+(?P<dcachesize_f>[^\s]+)\s+(?P<numfile>[^\s]+)\s+(?P<numfile_f>[^\s]+)\s+(?P<numiptent>[^\s]+)\s+(?P<numiptent_f>[^\s]+)\s+(?P<diskspace>[^\s]+)\s+(?P<diskspace_s>[^\s]+)\s+(?P<diskspace_h>[^\s]+)\s+(?P<diskinodes>[^\s]+)\s+(?P<diskinodes_s>[^\s]+)\s+(?P<diskinodes_h>[^\s]+)\s+(?P<laverage>[^\s]+)/m', $out, $matches);
 		}
 		// build a list of servers, and then send an update command to make usre that the server has information on all servers
-		foreach ($matches['ctid'] as $key => $id)
-		{
+		foreach ($matches['ctid'] as $key => $id) {
 			$server = array(
 				'veid' => $id,
 				'numproc' => $matches['numproc'][$key],
@@ -248,39 +234,32 @@ fi;\n";
 				if (isset($json_server['Remote display']) && isset($json_server['Remote display']['port']))
 					$servers[$json_server['ID']]['vnc'] = $json_server['Remote display']['port'];
 		}
-		foreach ($servers as $id => $server)
-		{
+		foreach ($servers as $id => $server) {
 			if ($id == 0)
 				continue;
 			$cmd = "export PATH=\"\$PATH:/bin:/usr/bin:/sbin:/usr/sbin\";if [ -e /vz/private/{$id}/root.hdd/DiskDescriptor.xml ];then ploop info /vz/private/{$id}/root.hdd/DiskDescriptor.xml 2>/dev/null | grep blocks | awk '{ print \$3 \" \" \$2 }'; else vzquota stat $id 2>/dev/null | grep blocks | awk '{ print \$2 \" \" \$3 }'; fi;";
 			//echo "Running $cmd\n";
 			$out = trim(`$cmd`);
-			if ($out != '')
-			{
+			if ($out != '') {
 				$disk = explode(' ', $out);
 				$servers[$id]['diskused'] = $disk[0];
 				$servers[$id]['diskmax'] = $disk[1];
 			}
 		}
-		if ($cpu_usage = @unserialize(`bash /root/cpaneldirect/cpu_usage.sh -serialize`))
-		{
-			foreach ($cpu_usage as $id => $cpu_data)
-			{
+		if ($cpu_usage = @unserialize(`bash /root/cpaneldirect/cpu_usage.sh -serialize`)) {
+			foreach ($cpu_usage as $id => $cpu_data) {
 				//$servers[$id]['cpu_usage'] = serialize($cpu_data);
 				$servers[$id]['cpu_usage'] = $cpu_data;
 			}
 		}
 		//print_r($servers);
 		$tips = trim(`/root/cpaneldirect/vps_get_ip_assignments.sh`);
-		if ($tips != '')
-		{
+		if ($tips != '') {
 			$tips = explode("\n", $tips);
-			foreach ($tips as $line)
-			{
+			foreach ($tips as $line) {
 				$parts = explode(' ', $line);
 				$ips[$parts[0]] = array();
-				foreach ($parts as $idx => $ip)
-				{
+				foreach ($parts as $idx => $ip) {
 					if ($idx == 0)
 						continue;
 					$ips[$parts[0]][] = $ip;
@@ -290,20 +269,13 @@ fi;\n";
 		}
 	}
 	//if (preg_match_all("/^[ ]*(?P<dev>[\w]+):(?P<inbytes>[\d]+)[ ]+(?P<inpackets>[\d]+)[ ]+(?P<inerrs>[\d]+)[ ]+(?P<indrop>[\d]+)[ ]+(?P<infifo>[\d]+)[ ]+(?P<inframe>[\d]+)[ ]+(?P<incompressed>[\d]+)[ ]+(?P<inmulticast>[\d]+)[ ]+(?P<outbytes>[\d]+)[ ]+(?P<outpackets>[\d]+)[ ]+(?P<outerrs>[\d]+)[ ]+(?P<outdrop>[\d]+)[ ]+(?P<outfifo>[\d]+)[ ]+(?P<outcolls>[\d]+)[ ]+(?P<outcarrier>[\d]+)[ ]+(?P<outcompressed>[\d]+)[ ]*$/im", file_get_contents('/proc/net/dev'), $matches))
-	if (preg_match_all("/^[ ]*([\w]+):\s*([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]*$/im", file_get_contents('/proc/net/dev'), $matches))
-	{
-		$bw = array(time(), 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+	if (preg_match_all("/^[ ]*([\w]+):\s*([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]*$/im", file_get_contents('/proc/net/dev'), $matches)) {
+		$bw = [time(), 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 		foreach ($matches[1] as $idx => $dev)
-		{
 			if (substr($dev, 0, 3) == 'eth')
-			{
 				for ($x = 1; $x < 16; $x++)
-				{
 					$bw[$x] += $matches[$x+1][$idx];
-				}
-			}
-		}
-		$bw_usage = array(
+		$bw_usage = [
 			'time' => $bw[0],
 			'bytes_in' => $bw[1],
 			'packets_in' => $bw[2],
@@ -317,11 +289,10 @@ fi;\n";
 			'packets_total' => $bw[2] + $bw[10],
 			'bytes_sec_total' => 0,
 			'packets_sec_total' => 0,
-		);
-		if (file_exists('/root/.bw_usage.last'))
-		{
+		]
+		if (file_exists('/root/.bw_usage.last')) {
 			$bw_last = unserialize(file_get_contents('/root/.bw_usage.last'));
-			$bw_usage_last = array(
+			$bw_usage_last = [
 				'time' => $bw_last[0],
 				'bytes_in' => $bw_last[1],
 				'packets_in' => $bw_last[2],
@@ -335,15 +306,11 @@ fi;\n";
 				'packets_total' => $bw_last[2] + $bw_last[10],
 				'bytes_sec_total' => 0,
 				'packets_sec_total' => 0,
-			);
+			]
 			$time_diff = $bw[0] - $bw_last[0];
 			foreach(array('bytes', 'packets') as $stat)
-			{
 				foreach (array('in','out','total') as $dir)
-				{
 					$bw_usage[$stat.'_sec_'.$dir] = ($bw_usage[$stat.'_'.$dir] - $bw_usage_last[$stat.'_'.$dir]) / $time_diff;
-				}
-			}
 		}
 		file_put_contents('/root/.bw_usage.last', serialize($bw));
 		$servers[0]['bw_usage'] = $bw_usage;
@@ -353,8 +320,7 @@ fi;\n";
 	//$speed = trim(`ethtool $(brctl show $(ip route |grep ^default | sed s#"^.*dev \([^ ]*\) .*$"#"\1"#g)  |grep -v "bridge id" | awk '{ print $4 }') |grep Speed: | sed -e s#"^.* \([0-9]*\).*$"#"\1"#g`);
 	if (in_array(trim(`hostname`), array("kvm1.trouble-free.net", "kvm2.interserver.net", "kvm50.interserver.net")))
 		$eth = 'eth1';
-	elseif (file_exists('/etc/debian_version'))
-	{
+	elseif (file_exists('/etc/debian_version')) {
 		if (file_exists('/sys/class/net/p2p1'))
 			$eth = 'p2p1';
 		elseif (file_exists('/sys/class/net/em1'))
@@ -374,27 +340,22 @@ fi;\n";
 	$found = false;
 	$lines = sizeof($cpuinfo);
 	$line = 0;
-	while ($found != true && $line < $lines)
-	{
+	while ($found != true && $line < $lines) {
 		$cpuline = $cpuinfo[$line];
-		if (substr($cpuline, 0, 5) == 'flags')
-		{
+		if (substr($cpuline, 0, 5) == 'flags') {
 			$flags = explode(' ', trim(substr($cpuline, strpos($cpuline, ':') + 1)));
 			$found = true;
-		} else {
+		} else
 			$line++;
-		}
 	}
 	sort($flags);
 	$flagsnew = implode(' ', $flags);
 	$flags = $flagsnew;
 	unset($flagsnew);
-
-	if (file_exists('/etc/redhat-release')) {
+	if (file_exists('/etc/redhat-release'))
 		preg_match('/^(?P<distro>[\w]+)( Linux)? release (?P<version>[\S]+)( .*)*$/i', file_get_contents('/etc/redhat-release'), $matches);
-	} else {
+	else
 		preg_match('/DISTRIB_ID=(?P<distro>[^<]+)<br>DISTRIB_RELEASE=(?P<version>[^<]+)<br>/i', str_replace("\n", '<br>', file_get_contents('/etc/lsb-release')), $matches);
-	}
 	$servers[0]['os_info'] = array(
 		'distro' => $matches['distro'],
 		'version' => $matches['version'],
