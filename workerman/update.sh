6d30db074db4beec626f9c827dac53f640870f4a
Joe Huss <detain@interserver.net>
2018-02-21 20:42:19 +0000

    for the suspend email a billing change was called for .. it will now properly skip the suspension calling on invoices w/ no repeat invoices tied to them

--- workerman/update.sh
+++ workerman/update.sh
@@ -2,7 +2,9 @@
 if [ "$(which composer)" = "" ]; then
     curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer --ansi;
 fi;
-if [ -e /etc/yum ]; then
+if [ -e /etc/apt ]; then
+	apt install -f php
+elif [ -e /etc/yum ]; then
 	yum install -y php php-cli php-bcmath php-devel php-gd php-process php-xml;
 fi
 sed s#"^memory_limit = .*$"#"memory_limit = 512M"#g -i /etc/php.ini
