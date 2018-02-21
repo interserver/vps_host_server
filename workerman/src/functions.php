024443aca5a9666bd95796f4705a5000fc8442e1
Joe Huss <detain@interserver.net>
2018-02-21 00:10:07 +0000

    fix

--- workerman/src/functions.php
+++ workerman/src/functions.php
@@ -6,7 +6,7 @@ include_once __DIR__.'/Data/xml2array.php';
 function vps_update_info_timer() {
 	global $global, $settings;
 	$task_connection = new AsyncTcpConnection('Text://127.0.0.1:55552');
-	$task_connection->send(json_encode(array('function' => 'async_hyperv_get_list', 'args' => array())]));
+	$task_connection->send(json_encode(array('function' => 'async_hyperv_get_list', 'args' => array())));
 	$task_connection->onMessage = function($task_connection, $task_result) use ($task_connection) {
 		 //var_dump($task_result);
 		 $task_connection->close();
