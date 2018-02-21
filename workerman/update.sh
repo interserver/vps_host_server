5a215a043e05808b4f156cf27fff28780d9e9d6c
Joe Huss <detain@interserver.net>
2018-02-21 01:17:07 +0000

    fixs

--- workerman/update.sh
+++ workerman/update.sh
@@ -3,7 +3,7 @@ if [ "$(which composer)" = "" ]; then
     curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer --ansi;
 fi;
 if [ -e /etc/yum ]; then
-	yum install -y php php-cli php-bcmath php-devel php-gd php-process;
+	yum install -y php php-cli php-bcmath php-devel php-gd php-process php-xml;
 fi
 sed s#"^memory_limit = .*$"#"memory_limit = 512M"#g -i /etc/php.ini
 composer update --with-dependencies -v -o --ansi --no-dev
