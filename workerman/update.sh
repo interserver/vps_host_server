d3e504cda7edd8f5d03c536d2484a1a287ad0554
Joe Huss <detain@interserver.net>
2018-02-23 00:19:49 +0000

    some work improving the communications and interactions between host hub and client

--- workerman/update.sh
+++ workerman/update.sh
@@ -3,9 +3,15 @@ if [ "$(which composer)" = "" ]; then
     curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer --ansi;
 fi;
 if [ -e /etc/apt ]; then
-	apt install -f php
+	. /etc/lsb-release ;
+	apt install -f php libssl-dev pkg-config php-pear;
+	if [ "$DISTRIB_CODENAME" = "trusty" ]; then 
+		apt install -y php5-dev php5-curl;
+	else
+		apt install -y php-dev php-curl; 
+	fi;
 elif [ -e /etc/yum ]; then
-	yum install -y php php-cli php-bcmath php-devel php-gd php-process php-xml;
+	yum install -y php php-cli php-bcmath php-devel php-gd php-process php-xml openssl-devel gcc php-curl libev libevent libev-devel libevent-devel php-pear;
 fi
 sed s#"^memory_limit = .*$"#"memory_limit = 512M"#g -i /etc/php.ini
 composer update --with-dependencies -v -o --ansi --no-dev
