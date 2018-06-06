55359d8b58dd2b1576c34d5628e82c6529fa0017
Joe Huss <detain@interserver.net>
2018-06-06 20:20:59 +0000

    moving message handling around a bit in the vps host connection timers

--- workerman/src/Events/onMessage.php
+++ workerman/src/Events/onMessage.php
@@ -15,15 +15,7 @@ return function($stdObject, $conn, $data) {
 		case 'login':
 			if ($data['ima'] == 'host')
 			{
-				if (!isset($stdObject->timers['vps_update_info']))
-					$stdObject->timers['vps_update_info'] = Timer::add(600, array($stdObject, 'vps_update_info'));
-				if (!isset($stdObject->timers['vps_get_traffic']))
-					$stdObject->timers['vps_get_traffic'] = Timer::add(60, array($stdObject, 'vps_get_traffic'));
-				if (!isset($stdObject->timers['vps_get_cpu']))
-				if (!isset($stdObject->timers['vps_get_list']))
-					$stdObject->timers['vps_get_list'] = Timer::add(600, array($stdObject, 'get_map_timer'));
-				$stdObject->vps_update_info();
-				$stdObject->get_map_timer();
+					$stdObject->setupTimers();
 			}
 			break;
 		case 'timers':
---
+++ workerman/src/Events/setupTimers.php
@@ -0,0 +1,19 @@
+<?php
+
+use Workerman\Worker;
+use Workerman\Lib\Timer;
+use Workerman\Connection\TcpConnection;
+use Workerman\Connection\AsyncTcpConnection;
+
+return function($stdObject, $conn, $data) {
+	$stdObject->conn = $conn;
+	$stdObject->vps_update_info();
+	$stdObject->get_map_timer();
+	if (!isset($stdObject->timers['vps_update_info']))
+		$stdObject->timers['vps_update_info'] = Timer::add(600, array($stdObject, 'vps_update_info'));
+	if (!isset($stdObject->timers['vps_get_traffic']))
+		$stdObject->timers['vps_get_traffic'] = Timer::add(60, array($stdObject, 'vps_get_traffic'));
+	if (!isset($stdObject->timers['vps_get_cpu']))
+	if (!isset($stdObject->timers['vps_get_list']))
+		$stdObject->timers['vps_get_list'] = Timer::add(600, array($stdObject, 'get_map_timer'));
+};
