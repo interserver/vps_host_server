739bcdf994d4c180fab2deff127d4531642fa1bc
Joe Huss <detain@interserver.net>
2018-06-12 17:26:06 +0000

    removing the disable/enable cron part of update virtuozzo

--- update_virtuozzo.sh
+++ update_virtuozzo.sh
@@ -11,18 +11,6 @@ function age() {
 }
 
 yum upgrade -y;
-echo "Disabling VPS Cron"
-crontab -l > crontab.txt ; 
-sed s#"^\(\*.*vps_cron.*\)$"#"\#\1"#g -i crontab.txt;
-crontab crontab.txt
-if [ "$(ps aux|grep root|grep vps|grep -v grep)" != "" ]; then
-  echo -n "Waiting for cron to stop"
-  while [ "$(ps aux|grep vps|grep -v grep)" != "" ]; do
-    sleep 10s;
-    echo -n .;
-  done
-  echo "done"
-fi
 vzpkg update metadata;
 vzpkg list -O | awk '{ print $1 }' | xargs -n 1 vzpkg fetch -O;
 vzlist -a -H | awk '{ print $1 }' |xargs -n 1 vzpkg update;
@@ -30,7 +18,3 @@ if [ ! -e ".cron_weekly.age" ] || [ $(age .cron_weekly.age) -ge 604800 ]; then
   vzpkg update cache --update-cache;
   touch .cron_weekly.age;
 fi
-echo "Enabling VPS Cron"
-sed s#"^\#\(\*.*vps_cron.*\)$"#"\1"#g -i crontab.txt;
-crontab crontab.txt
-rm -f crontab.txt
