c7d6e7427ed6edf2b5cf3684056cc7dc81745f00
Joe Huss <detain@interserver.net>
2018-07-03 01:03:18 +0000

    updates to requirements installer script

--- nagios-plugin-check_raid_prerequisites.sh
+++ nagios-plugin-check_raid_prerequisites.sh
@@ -6,17 +6,17 @@ if [ -e /etc/redhat-release ]; then
   if [ ! -e /usr/bin/scons ]; then
      rpm --install http://prdownloads.sourceforge.net/scons/scons-2.4.1-1.noarch.rpm
   fi;
+perldir="$(perl -V |grep -v "\.$" | tail -n 1 | sed s#" "#""#g)";
+rm -f ${perldir}/CPAN/Config.pm;
+/root/cpaneldirect/perl_cpan_setup.exp;
+	modules="Module::Build Test::Fatal Test::Requires Module::Implementation Params::Validate Monitoring::Plugin Module::Pluggable ExtUtils::MakeMaker ExtUtils::MakeMaker::CPANfile";
+	for i in $modules; do 
+	  #PERL_MM_USE_DEFAULT=1 perl -MCPAN -e "install $i" >&2
+	  PERL_MM_USE_DEFAULT=1 cpan $i >&2
+	done;
 elif [ -e /etc/apt ]; then 
   . /etc/lsb-release; 
   distro=ubuntu; 
   version=$DISTRIB_RELEASE;
-  apt-get install -y perl expect libyaml-dev libyaml-libyaml-perl libyaml-perl libyaml-cpp-dev libmonitoring-plugin-perl >&2;
+  apt-get install -y perl expect libyaml-dev libyaml-libyaml-perl libyaml-perl libyaml-cpp-dev libmonitoring-plugin-perl libmodule-pluggable-perl libmodule-build-perl libtest-fatal-perl libtest-requires-perl libmodule-implementation-perl libparams-validate-perl libextutils-makemaker-cpanfile-perl >&2;
 fi;
-perldir="$(perl -V |grep -v "\.$" | tail -n 1 | sed s#" "#""#g)";
-rm -f ${perldir}/CPAN/Config.pm;
-/root/cpaneldirect/perl_cpan_setup.exp;
-modules="Module::Build Test::Fatal Test::Requires Module::Implementation Params::Validate Monitoring::Plugin Module::Pluggable ExtUtils::MakeMaker ExtUtils::MakeMaker::CPANfile";
-for i in $modules; do 
-  #PERL_MM_USE_DEFAULT=1 perl -MCPAN -e "install $i" >&2
-  PERL_MM_USE_DEFAULT=1 cpan $i >&2
-done;
