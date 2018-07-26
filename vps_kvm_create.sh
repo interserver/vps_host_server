1c59446036cb82e45fb7eb4b32ed7afa96169d81
Joe Huss <detain@interserver.net>
2018-07-26 16:50:59 +0000

    fix

--- vps_kvm_create.sh
+++ vps_kvm_create.sh
@@ -58,7 +58,7 @@ fi
 error=0
 adjust_partitions=1
 export PREPATH="";
-if [ -e /etc/redhat-release ] && [ $(cat /etc/redhat-release| cut -d" " -f3 | cut -d"." -f1) -le 6 ]; then
+if [ -e /etc/redhat-release ] && [ "$(cat /etc/redhat-release| cut -d" " -f3 | cut -d"." -f1)" = "6" ]; then
 	if [ $(echo "$(e2fsck -V 2>&1 |head -n 1 | cut -d" " -f2 | cut -d"." -f1-2) * 100" | bc | cut -d"." -f1) -le 141 ]; then
 		if [ ! -e /opt/e2fsprogs/sbin/e2fsck ]; then
 			pushd $PWD;
