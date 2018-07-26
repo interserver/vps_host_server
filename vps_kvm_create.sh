f7f94f9b3c568e6a767f7c67212a5a9dc8d74e4a
Joe Huss <detain@interserver.net>
2018-07-26 16:47:50 +0000

    temp disable new image uplaod proceedure as its causing problems on some servers so just using old method for the moment

--- vps_kvm_create.sh
+++ vps_kvm_create.sh
@@ -137,14 +137,14 @@ else
 	if [ "$template" = "windows1" ]; then
 		template=windows2
 	fi
-	if [ -e "/${template}.img" ]; then
-		echo "Uploading $template Image"
-		virsh vol-upload $name "/${template}.img" --pool vz
-	elif [ -e "/${template}.img.gz" ]; then
-		echo "Uploading $template Image"
-		gunzip "/${template}.img.gz"
-		virsh vol-upload $name "/${template}.img" --pool vz
-	elif [ "${template:0:7}" = "http://" ] || [ "${template:0:8}" = "https://" ] || [ "${template:0:6}" = "ftp://" ]; then
+	#if [ -e "/${template}.img" ]; then
+	#	echo "Uploading $template Image"
+	#	virsh vol-upload $name "/${template}.img" --pool vz
+	#elif [ -e "/${template}.img.gz" ]; then
+	#	echo "Uploading $template Image"
+	#	gunzip "/${template}.img.gz"
+	#	virsh vol-upload $name "/${template}.img" --pool vz
+	if [ "${template:0:7}" = "http://" ] || [ "${template:0:8}" = "https://" ] || [ "${template:0:6}" = "ftp://" ]; then
 		adjust_partitions=0
 		echo "Downloading $template Image"
 		/root/cpaneldirect/vps_get_image.sh "$template"
