02eb1b1be8b658d49a3c2aca7c4163e08abcd10a
Joe Huss <detain@interserver.net>
2018-07-30 16:36:34 +0000

    updates to setup vnc script fixing issue with routes and reloading

--- vps_kvm_setup_vnc.sh
+++ vps_kvm_setup_vnc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin:/usr/X11R6/bin:/root/bin"
 name=$1
-myip="$(ifconfig $(ip route list | grep "^default" | sed s#"^default.*dev "#""#g | cut -d" " -f1)  |grep inet |grep -v inet6 | awk '{ print $2 }' | cut -d: -f2)"
+myip="$(ifconfig $(ip route list | grep "^default" | sed s#"^default.*dev "#""#g | head -n 1 | cut -d" " -f1)  |grep inet |grep -v inet6 | awk '{ print $2 }' | cut -d: -f2)"
 ip=$2
 if [ $# -ne 2 ]; then
  echo "Open VNC To IP"
@@ -35,7 +35,9 @@ elif [ "$(which virsh)" != "" ];then
     #else
       #echo "no spice port found for $myip, skipping"
     fi
-    if [ -e /etc/init.d/xinetd ]; then
+    if [ "$(which systemctl)" != "" ]; then
+       systemctl reload xinetd
+    elif [ -e /etc/init.d/xinetd ]; then
       /etc/init.d/xinetd reload >/dev/null 2>&1
     else
       service xinetd reload >/dev/null 2>&1
