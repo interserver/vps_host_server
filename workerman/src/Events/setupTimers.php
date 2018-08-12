9c9654573d3c2dd9ff9c10d5efb466d4dd38b97a
Joe Huss <detain@interserver.net>
2018-08-12 20:27:51 +0000

    updates killing old timers when they exist before creating hte new ones

--- workerman/src/Events/setupTimers.php
+++ workerman/src/Events/setupTimers.php
@@ -8,11 +8,13 @@ use Workerman\Connection\AsyncTcpConnection;
 return function($stdObject) {
 	$stdObject->vps_update_info();
 	$stdObject->get_map_timer();
-	if (!isset($stdObject->timers['vps_update_info']))
-		$stdObject->timers['vps_update_info'] = Timer::add($stdObject->config['timers']['vps_update_info'], array($stdObject, 'vps_update_info'));
-	if (!isset($stdObject->timers['vps_get_traffic']))
-		$stdObject->timers['vps_get_traffic'] = Timer::add($stdObject->config['timers']['vps_get_traffic'], array($stdObject, 'vps_get_traffic'));
-	//if (!isset($stdObject->timers['vps_get_cpu']))
-	if (!isset($stdObject->timers['vps_get_list']))
-		$stdObject->timers['vps_get_list'] = Timer::add($stdObject->config['timers']['get_map'], array($stdObject, 'get_map_timer'));
+	if (isset($stdObject->timers['vps_update_info']))
+		Timer::del($stdObject->timers['vps_update_info']);
+	$stdObject->timers['vps_update_info'] = Timer::add($stdObject->config['timers']['vps_update_info'], array($stdObject, 'vps_update_info'));
+	if (isset($stdObject->timers['vps_get_traffic']))
+		Timer::del($stdObject->timers['vps_get_traffic']);
+	$stdObject->timers['vps_get_traffic'] = Timer::add($stdObject->config['timers']['vps_get_traffic'], array($stdObject, 'vps_get_traffic'));
+	if (isset($stdObject->timers['vps_get_list']))
+		Timer::del($stdObject->timers['vps_get_list']);
+	$stdObject->timers['vps_get_list'] = Timer::add($stdObject->config['timers']['get_map'], array($stdObject, 'get_map_timer'));
 };
