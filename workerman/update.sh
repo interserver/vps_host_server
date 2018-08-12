8885b460672e01662c7504867c00aa7cfced204a
Joe Huss <detain@interserver.net>
2018-08-12 20:05:43 +0000

    lots of updates cleaning up the update script for hosts improving the event ev and swoole extension installers and the ssl settings a bit

--- workerman/update.sh
+++ workerman/update.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+
 function stop_service() {
 	./start.php stop
 	killall -9 vmstat
 }
+
 function check_svn() {
 	svnversion=1.9;
 	svnversionshort=$(echo "$svnversion" | sed s#"\."#""#g);
@@ -13,68 +15,129 @@ function check_svn() {
 			version=$(cat /etc/redhat-release  | sed s#"^.* \([0-9]\)\..*$"#"\1"#g); 
 			if [ $version -lt 70 ]; then 
 				echo -e "[WANdisco-svn${svnversionshort}]\nname=WANdisco SVN Repo ${svnversion}\nenabled=1\nbaseurl=http://opensource.wandisco.com/centos/${version}/svn-${svnversion}/RPMS/\$basearch/\ngpgcheck=1\ngpgkey=http://opensource.wandisco.com/RPM-GPG-KEY-WANdisco" > /etc/yum.repos.d/WANdisco-svn${svnversion}.repo; 
-				yum makecache; 
+				sudo yum makecache; 
 			else
 				rm -f /etc/yum.repos.d/WANdisco*; 
 			fi; 
-			yum upgrade -y subversion; 
+			sudo yum upgrade -y subversion; 
 		elif [ -e /etc/apt ]; then 
 			. /etc/lsb-release; 
 			distro=ubuntu;
 			version=$DISTRIB_RELEASE;
-			echo -e "# WANdisco Open Source Repo\ndeb http://opensource.wandisco.com/ubuntu ${DISTRIB_CODENAME} svn${svnversionshort}" > /etc/apt/sources.list.d/WANdisco-svn${svnversionshort}.list; 
-			apt-get update; 
-			apt-get install subversion -y; 
+			sudo echo -e "# WANdisco Open Source Repo\ndeb http://opensource.wandisco.com/ubuntu ${DISTRIB_CODENAME} svn${svnversionshort}" > /etc/apt/sources.list.d/WANdisco-svn${svnversionshort}.list; 
+			sudo apt-get update; 
+			sudo apt-get install subversion -y; 
 		fi;
 	fi;
 	if [ "$(which git)" = "" ]; then
 		if [ -e /etc/redhat-release ]; then
-			yum install -y git;
+			sudo yum install -y git;
 		else
-			apt-get install -y git;
+			sudo apt-get install -y git;
 		fi
 	fi;
 }
+
 function svn_up() {
 	svn update --accept theirs-full --username vpsclient --password interserver123 --trust-server-cert --non-interactive /root/cpaneldirect || rsync -av rsync://vpsadmin.interserver.net/vps/cpaneldirect/ /root/cpaneldirect/;
 }
+
 function check_composer() {
 	if [ "$(which composer)" = "" ]; then
-		curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer --ansi;
+		sudo curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer --ansi;
 	fi;
 }
+
 function check_php() {
 	if [ -e /etc/apt ]; then
 		. /etc/lsb-release ;
-		apt install -y -f php-cli libssl-dev pkg-config php-pear;
+		sudo apt install -y -f php-cli libssl-dev pkg-config php-pear;
 		if [ "$DISTRIB_CODENAME" = "trusty" ]; then 
-			apt install -y php5-dev php5-curl;
+			sudo apt install -y php5-dev php5-curl;
 		else
-			apt install -y php-dev php-curl php-pear libev4 libev-dev libev-libevent-dev php-bcmath php-curl php-xml php-bz2 php-zip php-mbstring php-imagick php-intl php-json php-soap; 
+			sudo apt install -y php-dev php-curl php-pear libev4 libev-dev libev-libevent-dev php-bcmath php-curl php-xml php-bz2 php-zip php-mbstring php-imagick php-intl php-json php-soap; 
 		fi;
 	elif [ -e /etc/yum ]; then
-		rpm -e libevent-devel libevent-headers libevent-doc
+		sudo rpm -e libevent-devel libevent-headers libevent-doc
 		#yum install -y php php-cli php-bcmath php-devel php-gd php-process php-xml php-curl php-pear;
-		yum install -y openssl-devel gcc libev libevent2 libev-devel libevent2-devel  || yum install openssl-devel gcc libev-devel libevent-devel libev libevent -y
+		sudo yum install -y openssl-devel gcc libev libevent2 libev-devel libevent2-devel  || yum install openssl-devel gcc libev-devel libevent-devel libev libevent -y
 	fi
+	inifile="$(php -i |grep 'Loaded Configuration' |awk '{ print $5 }')"
+	sudo sed s#"^memory_limit = .*$"#"memory_limit = 512M"#g -i "$inifile"
+}
+
+function check_php_event() {
+	if [ "$(php -m|grep event)" = "" ]; then
+		pecl download event
+		tar xvzf event-2.4.1.tgz 
+		cd event-2.4.1/
+		phpize
+		./configure --with-event-core --with-event-pthreads --with-event-extra --with-event-openssl --enable-event-sockets
+		make
+		sudo make install
+		cd ..
+		rm -rf event-2.4.1 event-2.4.1.tgz 
+		if [ -e /etc/apt ]; then
+			for i in /etc/php/*/mods-available; do 
+				sudo echo extension=event.so > $i/event.ini
+			done
+		else
+			sudo echo extension=event.so > /etc/php.d/event.ini
+		fi
+		sudo phpenmod -s ALL -v ALL event
+	fi
+}
+
+function check_php_ev() {
+	if [ "$(php -m|grep ev$)" = "" ]; then
+		pecl download ev
+		tar xvzf ev-1.0.4.tgz 
+		cd ev-1.0.4/
+		phpize
+		./configure --enable-ev 
+		make
+		sudo make install
+		cd ..
+		rm -rf ev-1.0.4 ev-1.0.4.tgz 
+		if [ -e /etc/apt ]; then
+			for i in /etc/php/*/mods-available; do 
+				sudo echo extension=ev.so > $i/ev.ini
+			done
+		else
+			sudo echo extension=ev.so > /etc/php.d/ev.ini
+		fi
+		sudo phpenmod -s ALL -v ALL ev
+	fi
+}
+
+function check_php_swoole() {
 	if [ "$(php -m|grep swoole)" = "" ]; then
-		git clone https://github.com/swoole/swoole-src.git
+		if [ $(php-config --version|cut -c1) -ge 7 ]; then
+			git clone https://github.com/swoole/swoole-src.git
+		else
+			wget https://github.com/swoole/swoole-src/archive/v1.10.5.tar.gz
+			tar xvzf v1.10.5.tar.gz
+			cd swoole-src-1.10.5
+			rm -f v1.10.5.tar.gz
+			mv -f swoole-src-1.10.5 swoole-src
+		fi
 		cd swoole-src
 		phpize
 		./configure --enable-sockets --enable-openssl --with-swoole
-		make && make install
+		make && sudo make install
 		cd ..
 		rm -rf swoole-src
 		if [ -e /etc/apt ]; then
-			echo extension=swoole.so > $(echo /etc/php/*/mods-available)/swoole.ini
+			for i in /etc/php/*/mods-available; do 
+				sudo echo extension=swoole.so > $i/swoole.ini
+			done
+			sudo phpenmod -v ALL -s ALL swoole
 		else
-			echo extension=swoole.so > /etc/php.d/swoole.ini
+			sudo echo extension=swoole.so > /etc/php.d/swoole.ini
 		fi
-		phpenmod -v ALL -s ALL swoole
 	fi
-	inifile="$(php -i |grep 'Loaded Configuration' |awk '{ print $5 }')"
-	sed s#"^memory_limit = .*$"#"memory_limit = 512M"#g -i "$inifile"
 }
+
 function composer_up() {
 	composer install --no-dev
 	cat vendor/detain/phpsysinfo/phpsysinfo.ini.new > vendor/detain/phpsysinfo/phpsysinfo.ini
@@ -85,5 +148,8 @@ function composer_up() {
 check_svn
 svn_up
 check_php
+check_php_event
+check_php_ev
+check_php_swoole
 check_composer
 composer_up
