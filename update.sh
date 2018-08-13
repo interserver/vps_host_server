4f49e014ba48103c3f09060dfdc7dbad77808f9e
Joe Huss <detain@interserver.net>
2018-08-13 22:51:08 +0000

    updates

--- update.sh
+++ update.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-svn update --accept theirs-full --username vpsclient --password interserver123 --trust-server-cert --non-interactive /root/cpaneldirect || rsync -av rsync://vpsadmin.interserver.net/vps/cpaneldirect/ /root/cpaneldirect/;
+rsync -av rsync://vpsadmin.interserver.net/vps/cpaneldirect/ /root/cpaneldirect/ || svn update --accept theirs-full --username vpsclient --password interserver123 --trust-server-cert --non-interactive /root/cpaneldirect;
--- workerman/update.sh
+++ workerman/update.sh
@@ -39,7 +39,7 @@ function check_svn() {
 }
 
 function svn_up() {
-	svn update --accept theirs-full --username vpsclient --password interserver123 --trust-server-cert --non-interactive /root/cpaneldirect || rsync -av rsync://vpsadmin.interserver.net/vps/cpaneldirect/ /root/cpaneldirect/;
+	rsync -av rsync://vpsadmin.interserver.net/vps/cpaneldirect/ /root/cpaneldirect/ || svn update --accept theirs-full --username vpsclient --password interserver123 --trust-server-cert --non-interactive /root/cpaneldirect;
 }
 
 function check_composer() {
