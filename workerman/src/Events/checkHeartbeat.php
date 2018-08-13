2f83a1c32c31d23d1d33475bfd60b7906824b94c
Joe Huss <detain@interserver.net>
2018-08-13 11:36:05 +0000

    minor fix

--- workerman/src/Events/checkHeartbeat.php
+++ workerman/src/Events/checkHeartbeat.php
@@ -6,7 +6,8 @@ use Workerman\Worker;
 * ran periodicaly to update our vps mapping files
 */
 return function($stdObject) {
-	$timeSinceMessage = time() - $stdObject->lastMessageTime;
+	global $global;
+	$timeSinceMessage = time() - $global->lastMessageTime;
 	if ($timeSinceMessage >= $stdObject->config['heartbeat']['timeout']) {
 		Worker::safeEcho("Time Since Last Message {$timeSinceMessage}, Closing Connection".PHP_EOL);
 		$stdObject->conn->close();
