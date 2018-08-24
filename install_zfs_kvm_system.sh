ac684e312b2f8b05c81d12ef4bc907b042318e11
Joe Huss <detain@interserver.net>
2018-08-24 22:34:00 +0000

    updates to zfs system installer

--- install_zfs_kvm_system.sh
+++ install_zfs_kvm_system.sh
@@ -1,10 +1,37 @@
 #!/bin/bash
+# set HISTFILESIZE and HISTSIZE to unlimited 
+sed s#"^\(HIST.*SIZE\)=.*$"#"\1="#g -i /root/.bashrc
+# load ubuntu distro version variables
+. /etc/lsb-release
+# replace /etc/apt/sources.list with condensed and in most cases better version
+echo "
+deb http://us.archive.ubuntu.com/ubuntu/ ${DISTRIB_CODENAME} main restricted universe multiverse
+deb http://us.archive.ubuntu.com/ubuntu/ ${DISTRIB_CODENAME}-updates main restricted universe multiverse
+deb http://us.archive.ubuntu.com/ubuntu/ ${DISTRIB_CODENAME}-backports main restricted universe multiverse
+deb http://us.archive.ubuntu.com/ubuntu/ ${DISTRIB_CODENAME}-proposed main restricted universe multiverse 
+deb http://security.ubuntu.com/ubuntu ${DISTRIB_CODENAME}-security main restricted universe multiverse
+deb http://archive.canonical.com/ubuntu ${DISTRIB_CODENAME} partner
+
+deb-src http://us.archive.ubuntu.com/ubuntu/ ${DISTRIB_CODENAME} main restricted universe multiverse
+deb-src http://us.archive.ubuntu.com/ubuntu/ ${DISTRIB_CODENAME}-updates main restricted universe multiverse
+deb-src http://us.archive.ubuntu.com/ubuntu/ ${DISTRIB_CODENAME}-backports main restricted universe multiverse
+deb-src http://us.archive.ubuntu.com/ubuntu/ ${DISTRIB_CODENAME}-proposed main restricted universe multiverse 
+deb-src http://security.ubuntu.com/ubuntu ${DISTRIB_CODENAME}-security main restricted universe multiverse
+deb-src http://archive.canonical.com/ubuntu ${DISTRIB_CODENAME} partner
+" > /etc/apt/sources.list;
+# update system
+apt-get update;
+apt-get autoremove -y;
+# ensure we dont have lxc (installed by default) related packages floating around
+apt-get remove -y --purge lxd lxd-client  lxcfs liblxc-common liblxc1;
 pkgs="
 bash-completion
 command-not-found
 imagemagick-6.q16
 ioping
 isc-dhcp-server
+libmodule-pluggable-perl
+libmonitoring-plugin-perl;
 libvirt-clients
 libvirt-daemon-driver-storage-zfs
 libvirt-dev
@@ -31,4 +58,19 @@ zfs-initramfs
 zfsutils-linux
 zfs-zed
 "
+# install some required packages
 apt-get install -y $pkgs 
+# update system to latest packages
+apt-get dist-upgrade -y;
+# remove outdated packages/kernels
+apt-get autoremove -y;
+cd /root/cpaneldirect
+# setup libvirt zfs / lvm storage pool
+./create_libvirt_storage_pools.sh
+cd /root/cpaneldirect/workerman
+# install some additional prerequisites
+./update.sh
+apt-get autoremove -y;
+apt-get clean;
+ldconfig;
+updatedb;
