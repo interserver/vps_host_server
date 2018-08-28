7df71472363f16bf7358847fc51a6ce78a8797ce
Joe Huss <detain@interserver.net>
2018-08-28 06:18:40 +0000

    script to build the various templates in image(raw) or qcow2 format , and update them, supports many features

---
+++ build_kvm_templates.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+IFS="
+"
+ext=qcow2
+format=qcow2
+if [ "$1" = "" ]; then
+	echo "$0 <raw|qcow2>"
+	echo " raw|qcow2 - specifies the output image format, defaults to qcow2, craetes .img/.qcow2 image files"
+	exit
+fi
+format=$1
+ext=$1
+if [ "$1" = "raw" ]; then
+	ext=img
+fi
+shift
+for i in $(virt-builder -l|sort -n); do
+	tag="$(echo "$i"|cut -d" " -f1)"
+	arch="$(echo "$i"|awk '{ print $2 }')"
+	archfile="$arch";
+	label="$(echo "$i"|sed s#"^[^ ]* *[^ ]* *"#""#g)"
+	os="$(echo "$tag"|cut -d- -f1)"
+	cmd="nice -n -19 ionice -c 2 -n 0 virt-builder ${tag} --network --colors -m 1024 --smp 8 --format ${format} --arch ${arch} -o ${tag}.${ext} --root-password 'password:interserver123' --ssh-inject 'root:string:from=\"66.45.228.251\" ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAgEAvuKNsgUCyIoXcpiYkfOikuzlY1TlGGKgU6jMqEu/abStxgncwIX6eV19F5WAl8WYFbpOaolIFAR1Slxd2t7FuSK9B9BGqLNYdhwOLd75EPK71gAbnE2proZvOkuVSNb6Eq6ZHzlWiRVISXZyeGfMiJWr8/BDaIOJQaUUJ5/PcLOcuvpQxqslCninf2usswNQ6feRgYRbebgY6ydBuWpvf1moTxBogAVkh5cvdmGFmFlK5L2OMnJJgfwaLHkE//F60CU5LTaZPMuK/DEM0TyPBKdNAR+4oNiw3NdX/CzCq8VPZyjaIpNkGCsMgZGC4gYcY7TXSOek+870ONGaPKKcQJVJe3IE48zeGQSAUe4FoZwoGVvOMuyM1Lh7986Q6Co8zLiGUOfvfD08kmsCtRuRhA04VigVKEEY/b1zS8T4wC1slb77HhbTL+Q0rF84rh0m0pZ2BFUDwpM64shsTfy7JVr8akN7A68UMA5yT/G7U0o3YsZW/Q0dmu/KaOv/s1sJ1Fhie/om5qsg31qZr1R9GyiOCq3qB5ZC8J8sH3ZKhHEH5ulO6nf6J02WIYJJUuIu2CSqlsvOWNwgp5z1H2T0HA407cetqRcGH+4ymBvXiLcPZTRi5wO/QGBX1NvyNP2MFaASeNm+EIvWXlQVVXnHIT5UdPLYHVv+L+YHkOT185k= root@tech.trouble-free.net'"
+	if [ "$arch" != "x86_64" ] || [ "$os" = "freebsd" ]; then
+		continue
+	fi;
+	case $os in
+	"centos")
+		h=mirror.trouble-free.net cmd="${cmd} --edit '/etc/yum.repos.d/CentOS-Base.repo: s{^mirrorlist=}{#mirrorlist=}; s{^#baseurl=}{baseurl=}; s{mirror.centos.org}{mirror.trouble-free.net};' --append-line '/etc/hosts:$(host $h|grep "has address"|head -n 1|cut -d" " -f4) $h' --selinux-relabel --update";;
+	"fedora")
+		for h in mirrors.fedoraproject.org dl.fedoraproject.org mirrors.rit.edu mirrors.kernel.org; do
+			cmd="${cmd} --append-line '/etc/hosts:$(host $h|grep "has address"|head -n 1|cut -d" " -f4) $h'";
+		done;
+		cmd="${cmd} --selinux-relabel --update";;
+	"opensuse")
+		h=download.opensuse.org	cmd="${cmd} --append-line '/etc/hosts:$(host $h|grep "has address"|head -n 1|cut -d" " -f4) $h' --update";;
+	"scientificlinux")
+		h=ftp.scientificlinux.org cmd="${cmd} --append-line '/etc/hosts:$(host $h|grep "has address"|head -n 1|cut -d" " -f4) $h' --update";;
+	"debian" | "ubuntu")
+		cmd="${cmd} --firstboot-command 'dpkg-reconfigure openssh-server' --update";;
+	esac;
+	echo "Building/Updating Tag: ${tag}  Arch: ${arch}  Label: ${label}  With: ${cmd}"
+	eval $cmd $*;
+	if [ ! -e "${tag}.${ext}" ]; then
+		echo $tag >> errors.txt
+	fi
+done
