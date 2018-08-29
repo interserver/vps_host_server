a297fed7e8b122976cd5ca6e6e6da0e7d0732cda
Joe Huss <detain@interserver.net>
2018-08-29 12:33:32 +0000

    ok when running the script now it will make sure each ip is valid ..  if it gets to an ip that is not valid .  it will   first check to see if  a .backup file exists, if so it will restore the backup and then break out of updating the file  and restart dhcp,   if a backup file did not exist for some reason it would just skip over that line entirely and keep writing the file  as normal

--- vps_kvm_update_dhcpd.sh
+++ vps_kvm_update_dhcpd.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 export PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin:/usr/X11R6/bin:/root/bin";
+
+# Test an IP address for validity, Usage:
+#      valid_ip IP_ADDRESS
+#      if [[ $? -eq 0 ]]; then echo good; else echo bad; fi
+#  or  if valid_ip IP_ADDRESS; then echo good; else echo bad; fi
+function valid_ip(){
+	local  ip=$1
+	local  stat=1
+	if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+		OIFS=$IFS
+		IFS='.'
+		ip=($ip)
+		IFS=$OIFS
+		[[ ${ip[0]} -le 255 && ${ip[1]} -le 255 && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
+		stat=$?
+	fi
+	return $stat
+}
+
 if [ "$(crontab -l|grep qs_cron)" != "" ]; then
 	url="https://myquickserver2.interserver.net/qs_queue.php"
 else
@@ -13,6 +32,10 @@ if [ -e /root/cpaneldirect/vps.mainips ]; then
 		DHCPVPS=/etc/dhcp/dhcpd.vps;
 		/bin/rm -f /etc/dhcpd.vps;
 		ln -s /etc/dhcp/dhcpd.vps /etc/dhcpd.vps;
+	elif [ -d /etc/dhcp ]; then
+		mv -f /etc/dhcpd.vps /etc/dhcp/dhcpd.vps;
+		ln -s /etc/dhcp/dhcpd.vps /etc/dhcpd.vps;
+		DHCPVPS=/etc/dhcp/dhcpd.vps;
 	else
 		DHCPVPS=/etc/dhcpd.vps;
 	fi;
@@ -24,7 +47,14 @@ if [ -e /root/cpaneldirect/vps.mainips ]; then
 		mac=`virsh dumpxml $user | grep "mac" | grep address | grep : | cut -d\' -f2`;
 		id="$(echo "$user" | sed s#"[[:alpha:]]"#""#g)"
 		ip="$(grep "^$id:" /root/cpaneldirect/vps.mainips | cut -d: -f2-)";
-		echo "host $user { hardware ethernet $mac; fixed-address $ip;}" >> ${DHCPVPS};
+		if valid_ip $ip; then
+				echo "host $user { hardware ethernet $mac; fixed-address $ip;}" >> ${DHCPVPS};
+		else
+			if [ -e ${DHCPVPS}.backup ]; then
+				/bin/mv -f ${DHCPVPS}.backup ${DHCPVPS};
+				break;
+			fi;
+		fi;
 	done;
 	if [ ! -e /etc/init.d/dhcpd ] && [ -e /etc/init.d/isc-dhcp-server ]; then
 		/etc/init.d/isc-dhcp-server restart;
