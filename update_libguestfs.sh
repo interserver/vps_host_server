8e0ff22d0294f7cc7b6ed4ee3748b712a168ceff
Joe Huss <detain@interserver.net>
2018-08-29 00:02:05 +0000

    added a script to update libguestfs package.. super awesome code/script

---
+++ update_libguestfs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+if [ "$(which equivs 2>/dev/null)" = "" ]; then apt-get install -y equivs; fi;
+if [ "$(which dpkg-source 2>/dev/null)" = "" ]; then apt-get install -y dpkg-dev; fi;
+if [ "$(which mk-build-deps 2>/dev/null)" = "" ]; then apt-get install -y devscripts; fi;
+latest="$(curl -s "http://us.archive.ubuntu.com/ubuntu/pool/universe/libg/libguestfs/"|grep "libguestfs_.*dsc"|cut -d\" -f8|sort -nr|head -n 1|sed s#"^libguestfs_\(.*\).dsc$"#"\1"#g)"
+installed="$(dpkg -l |grep libguestfs0|awk '{ print $3 }'|cut -d: -f2)"
+echo "Latest version: $latest     Installed Version: $installed"
+if [ "$latest" = "$installed" ]; then echo "No Update Needed"; exit; fi
+wget "http://us.archive.ubuntu.com/ubuntu/pool/universe/libg/libguestfs/libguestfs_${latest}.dsc" -O "libguestfs_${latest}.dsc"
+for i in $(cat libguestfs_${latest}.dsc|grep "^ [^ ]* [0-9]* "|awk '{ print $3 }'|sort|uniq); do
+        wget "http://us.archive.ubuntu.com/ubuntu/pool/universe/libg/libguestfs/$i" -O "$i"
+done
+dir="$(echo "libguestfs-${latest}"|cut -d- -f1-2)"
+rm -rf $dir
+dpkg-source -x "libguestfs_${latest}.dsc"
+cd $dir
+echo y|mk-build-deps -i || exit
+rm -f libguestfs-build-deps_${latest}_amd64.deb
+debuild || exit
+apt-get purge -y --auto-remove libguestfs-build-deps
+cd .. && \
+packages="$(grep "^Package" debian/control |cut -d" " -f2)" \
+installedpkgs="$(echo "$packages"|sed -e s#" $"#""#g -e s#" "#" -e "#g)|awk '{ print $2 }'|cut -d: -f1)" \
+upgradepkgs="$(eval dpkg -l |grep -e $(echo "${installedpkgs}"|tr "\n" " "|sed s#" "#"_${latest}\*deb "#g)" && \
+eval dpkg --install libguestfs-tools_${latest}_amd64.deb ${upgradepkgs};
