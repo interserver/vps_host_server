b670a1027995880f05f84174c6c6ae4b2ff2c91d
Joe Huss <detain@interserver.net>
2018-09-19 13:14:24 +0000

    updatesswapping out a efew calls so when theres a connect error the program dies and is reloaded to workaround the one sec key upgrade issue

--- workerman/src/Events/onClose.php
+++ workerman/src/Events/onClose.php
@@ -3,7 +3,7 @@ use Workerman\Worker;
 
 return function ($stdObject, $conn) {
 	echo 'Connection Closed, Shutting Down'.PHP_EOL;
-	//$conn->close();
-	$conn->reConnect(5);
-	//Worker::stopAll();
+	Worker::stopAll();
+	//$conn->reconnect(5);
+	$conn->close();
 };
