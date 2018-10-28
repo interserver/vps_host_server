c58674ed213aadf6e0e76698bda66c1927e77f45
Joe Huss <detain@interserver.net>
2018-10-28 03:33:09 +0000

    created a function for adding a timer and reworked the timer setups to use it cuttin back on the logic/dfificulty and added a vps get traffic call at the eginning.. also added more logging

---
+++ workerman/src/Events/addTimer.php
@@ -0,0 +1,20 @@
+<?php
+
+use Workerman\Worker;
+use Workerman\Lib\Timer;
+use Workerman\Connection\TcpConnection;
+use Workerman\Connection\AsyncTcpConnection;
+
+return function ($stdObject, $name, $interval = false, $callable = false) {
+    Worker::safeEcho("addTimer called with ({$name}, ".var_export($callable, true).", ".var_export($interval, true).") called timer set? ".var_export(isset($stdObject->timers[$name]), true));
+    if (isset($stdObject->timers[$name])) {
+        Worker::safeEcho("addTimer deleting timer {$name} id {$stdObject->timers[$name]}");
+		Timer::del($stdObject->timers[$name]);
+	}
+    if ($callable === false)
+        $callable = array($stdObject, $name);
+    if ($interval === false)
+        $interval = $stdObject->config['timers'][$name];
+	$stdObject->timers[$name] = Timer::add($interval, $callable);
+    Worker::safeEcho("addTimer adding timer {$name} every {$interval} got timer id {$stdObject->timers[$name]}");
+};
--- workerman/src/Events/setupTimers.php
+++ workerman/src/Events/setupTimers.php
@@ -8,20 +8,9 @@ use Workerman\Connection\AsyncTcpConnection;
 return function ($stdObject) {
 	$stdObject->vps_update_info();
 	$stdObject->get_map_timer();
-	if (isset($stdObject->timers['vps_update_info'])) {
-		Timer::del($stdObject->timers['vps_update_info']);
-	}
-	$stdObject->timers['vps_update_info'] = Timer::add($stdObject->config['timers']['vps_update_info'], array($stdObject, 'vps_update_info'));
-	if (isset($stdObject->timers['vps_get_traffic'])) {
-		Timer::del($stdObject->timers['vps_get_traffic']);
-	}
-	$stdObject->timers['vps_get_traffic'] = Timer::add($stdObject->config['timers']['vps_get_traffic'], array($stdObject, 'vps_get_traffic'));
-	if (isset($stdObject->timers['vps_get_list'])) {
-		Timer::del($stdObject->timers['vps_get_list']);
-	}
-	$stdObject->timers['vps_get_list'] = Timer::add($stdObject->config['timers']['get_map'], array($stdObject, 'get_map_timer'));
-	if (isset($stdObject->timers['check_interval'])) {
-		Timer::del($stdObject->timers['check_interval']);
-	}
-	$stdObject->timers['check_interval'] = Timer::add($stdObject->config['heartbeat']['check_interval'], array($stdObject, 'checkHeartbeat'));
+    $stdObject->vps_get_traffic();
+    $stdObject->addTimer('vps_update_info');
+    $stdObject->addTimer('vps_get_traffic');
+    $stdObject->addTimer('vps_get_list', $stdObject->config['timers']['get_map']);
+    $stdObject->addTimer('check_interval', $stdObject->config['heartbeat']['check_interval'], array($stdObject, 'checkHeartbeat'));
 };
