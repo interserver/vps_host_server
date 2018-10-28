2267a700e5721ad3a574c2faf6c53c258a81e4e8
Joe Huss <detain@interserver.net>
2018-10-28 03:45:08 +0000

    fix

--- workerman/src/Events/vps_get_traffic.php
+++ workerman/src/Events/vps_get_traffic.php
@@ -1,5 +1,7 @@
 <?php
 
+use Workerman\Worker;
+
 return function ($stdObject) {
     Worker::safeEcho("vps_get_traffic [0] called, calling get_vps_iptables_traffic\n");;
 	$totals = $stdObject->get_vps_iptables_traffic();
