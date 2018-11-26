1b998d5de4a58538adabcf1227d38768db4cdc65
Joe Huss <detain@interserver.net>
2018-11-26 13:12:54 -0500

    Updated the convert_id_to_mac scripts to now include a help screen when called with no params, and support for an optional module parameter which can trigger using a different-but-still-for-vms mac addresss prefix

--- convert_id_to_mac.php
+++ convert_id_to_mac.php
@@ -1,6 +1,34 @@
 #!/usr/bin/env php
 <?php
+/*
+ VMWare MACs
+  00:05:56
+  00:50:56
+  00:0C:29
+  00:1C:14
+ Xen MACs
+  00:16:3E
+*/
+if ($_SERVER['argc'] == 1) {
+    echo "Syntax: {$_SERVER['argv'][0]} <id> [module]
+    id - the Service ID number
+    module - vps or quickservers, defaults to vps, optional module this service is under";
+    exit;
+}
 $id = $_SERVER['argv'][1];
+$prefix = '00:16:3E';
+if (isset($_SERVER['argv'][2])) {
+    switch ($_SERVER['argv'][2]) {
+        case 'qs':
+        case 'quickservers':
+            $prefix = '00:0C:29';
+            break;
+        case 'vps':
+        default:
+            $prefix = '00:16:3E';
+            break;
+    }
+}
 $suffix = strtoupper(sprintf("%06s", dechex($id)));
-$mac = '00:16:3E:'.substr($suffix, 0, 2).':'.substr($suffix, 2, 2).':'.substr($suffix, 4, 2);
+$mac = $prefix.':'.substr($suffix, 0, 2).':'.substr($suffix, 2, 2).':'.substr($suffix, 4, 2);
 echo $mac.PHP_EOL;
--- convert_id_to_mac.sh
+++ convert_id_to_mac.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
-id=$*
+# VMWare MACs
+#  00:05:56
+#  00:50:56
+#  00:0C:29
+#  00:1C:14
+# Xen MACs
+#  00:16:3E
+if [ $# -eq 0 ]; then
+    echo "Syntax: ${0} <id> [module]
+    id - the Service ID number
+    module - vps or quickservers, defaults to vps, optional module this service is under"
+    exit
+fi
+id=$1
+if [ $# -eq 2 ]; then
+    if [ "$2" = "qs" ] || [ "$2" = "quickservers" ]; then
+        prefix="00:0C:29"
+    else
+        prefix="00:16:3E"
+    fi
+else
+    prefix="00:16:3E"
+fi
 s="$(printf "%06s" $(echo "obase=16; $id"|bc)|sed s#" "#"0"#g)"
-mac="00:16:3E:${s:0:2}:${s:2:2}:${s:4:2}"
+mac="${prefix}:${s:0:2}:${s:2:2}:${s:4:2}"
 echo $mac;
