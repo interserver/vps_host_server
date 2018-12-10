3550e6a81560b155affea3f9d2746993c891d928
Joe Huss <detain@interserver.net>
2018-12-10 06:35:27 -0500

    updates to onclose event

--- workerman/src/Events/onClose.php
+++ workerman/src/Events/onClose.php
@@ -3,10 +3,15 @@ use Workerman\Worker;
 use Workerman\Connection\AsyncTcpConnection;
 
 return function ($stdObject, AsyncTcpConnection $conn) {
-	echo 'Connection Closed, Shutting Down'.PHP_EOL;
-	echo exec('php '.__DIR__.'/../../start.php stop').PHP_EOL;
-	//$conn->reconnect(5);
-	$conn->close();
-	Worker::stopAll();
-	Worker::exitAndClearAll();
+    $reload = true;
+    if ($reload == true) {
+        Worker::safeEcho('Connection Closed, Reconnecting.'.PHP_EOL);
+        $conn->reconnect(5);
+    } else {
+	    Worker::safeEcho('Connection Closed, Shutting Down'.PHP_EOL);
+	    Worker::safeEcho(exec('php '.__DIR__.'/../../start.php stop').PHP_EOL);
+	    $conn->close();
+	    Worker::stopAll();
+	    Worker::exitAndClearAll();
+    }
 };
