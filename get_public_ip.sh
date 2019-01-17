98c3068071a86e56c2e879f7911b1fec9b463f98
Joe Huss <detain@interserver.net>
2019-01-17 05:01:25 -0500

    updates adding an additional fallback ip lookup and some other tweaks

--- get_public_ip.sh
+++ get_public_ip.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 unset ip;
 if [ "$(which dig)" != "" ]; then
-  ip="$(dig +short myip.opendns.com @resolver1.opendns.com)";
+  export ip="$(dig +short myip.opendns.com @resolver1.opendns.com)";
 fi;
 for s in ipecho.net/plain icanhazip.com ifconfig.me; do
   if [ ! -z $ip ] && [ "$(which curl)" != "" ]; then
-    ip="$(curl -s http://${s})";
+    export ip="$(curl -s http://${s})";
   fi;
   if [ ! -z $ip ] && [ "$(which wget)" != "" ]; then
-    ip="$(wget http://${s} -O - -q)";
+    export ip="$(wget http://${s} -O - -q)";
   fi;
 done;
+if [ ! -z $ip ] || [ "$(echo "${ip}"|grep "[0-9]*\.[0-9]")" = "" ]; then
+    export ip="$(ifconfig $(ip route list | grep "^default" | sed s#"^default.*dev "#""#g | cut -d" " -f1)  |grep inet |grep -v inet6 | awk "{ print \$2 }" | cut -d: -f2)"
+fi
