bf9bbc125300edd3e13f5b259a4d18b32ce4fa16
Joe Huss <detain@interserver.net>
2019-05-10 07:36:02 -0400

    updates

--- workerman/src/Events/checkHeartbeat.php
+++ workerman/src/Events/checkHeartbeat.php
@@ -6,6 +6,7 @@ use Workerman\Worker;
 * ran periodicaly to update our vps mapping files
 */
 return function ($stdObject) {
+    Worker::safeEcho("[HeartBeat] Check starting".PHP_EOL);
     /**
     * @var \GlobalData\Client
     */
@@ -14,6 +15,7 @@ return function ($stdObject) {
 	if ($timeSinceMessage >= $stdObject->config['heartbeat']['timeout']) {
 		Worker::safeEcho("Time Since Last Message {$timeSinceMessage}, Closing Connection".PHP_EOL);
 		$stdObject->conn->close();
+        Worker::stopAll();
 	} elseif ($timeSinceMessage >= $stdObject->config['heartbeat']['ping_when_silent_for']) {
 		//Worker::safeEcho("Time Since Last Message {$timeSinceMessage}, Sending Ping".PHP_EOL);
 		$stdObject->sendPing();
