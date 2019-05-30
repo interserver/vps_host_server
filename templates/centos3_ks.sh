edd18ee24efcdc967e6971238d40e4a20515bdb8
Joe Huss <detain@interserver.net>
2019-05-30 05:00:22 -0400

    removing some old and unused dirs

--- templates/centos3_ks.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-python -c 'import crypt,getpass; print(crypt.crypt(getpass.getpass(), crypt.mksalt(crypt.METHOD_SHA512)))'
-
--- templates/install_ksvalidator.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ -e /etc/redhat-release ] ; then
-	yum install pykickstart -y;
-elif [ -e /etc/apt ]; then
-	apt-get install python-pykickstart -y;
-fi;
-
