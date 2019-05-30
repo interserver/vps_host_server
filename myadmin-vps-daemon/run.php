edc858d56916b31555830fd178b2a078c12f5643
Joe Huss <detain@interserver.net>
2019-05-30 05:01:48 -0400

    removing some old unused files

--- myadmin-vps-daemon/run.php
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env php
-<?php
-require_once 'config.php';
-require_once 'error_handlers.php';
-
-use MyAdmin\Daemon;
-
-// The run() method will start the daemon event loop.
-MyAdmin\Daemon\Poller::getInstance()->run();
