fd1552151128b7783e4a18c92178c7ac2f44d075
Joe Huss <detain@interserver.net>
2019-09-26 08:39:43 -0400

    updates adding support for lxd testing and installls

---
+++ templates/install_lxd.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+vps=vps230617
+myip=70.44.33.193
+root="$2"
+ip=162.250.126.182
+template="$1"
+mac="00:16:3e:03:84:d9"
+
+export PATH="$PATH:/usr/sbin:/sbin:/bin:/usr/bin:";
+lxc stop -f ${vps};
+lxc delete -f ${vps};
+
+cp -f /etc/lxc/dnsmasq.conf /etc/lxc/dnsmasq.conf.backup;
+cat /etc/lxc/dnsmasq.conf.backup |grep -v -e ",${ip}$" -e "=${mac}," -e "=${vps}," > /etc/lxc/dnsmasq.conf;
+echo "dhcp-host=${mac},${ip}" >> /etc/lxc/dnsmasq.conf;
+killall -HUP dnsmasq
+lxc init "images:$template" ${vps}
+lxc config set ${vps} limits.memory 2048MB;
+lxc config set ${vps} limits.cpu 1;
+lxc config set ${vps} volatile.eth0.hwaddr ${mac};
+lxc network attach br0 ${vps} eth0
+lxc config device set ${vps} eth0 ipv4.address ${ip}
+lxc config device set ${vps} eth0 security.mac_filtering true
+lxc config device add ${vps} root disk path=/ pool=lxd size=30720GB;
+lxc start ${vps}
+lxc exec ${vps} -- bash -c "apt update ; apt install openssh-server -y ; sed s#'^\#*PermitRootLogin .*$'#'PermitRootLogin yes'#g -i /etc/ssh/sshd_config ; systemctl restart sshd;"
+lxc exec ${vps} -- echo root:'${root}' | chpasswd;
+lxc exec ${vps} -- locale-gen --purge en_US.UTF-8 && \
+lxc exec ${vps} -- bash -c "echo -e 'LANG=\"en_US.UTF-8\"\nLANGUAGE=\"en_US:en\"\n' > /etc/default/locale"
+
---
+++ templates/test_lxd.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+templates="ubuntu/14.04/amd64
+ubuntu/16.04/amd64
+ubuntu/18.04/amd64
+ubuntu/18.10/amd64
+oracle/6/amd64
+oracle/7/amd64
+opensuse/15.0/amd64
+opensuse/42.3/amd64
+fedora/26/amd64
+fedora/27/amd64
+fedora/28/amd64
+debian/10/amd64
+debian/8/amd64
+debian/sid/amd64
+debian/9/amd64
+debian/7/amd64
+centos/6/amd64
+centos/7/amd64"
+IFS="
+"
+for i in $templates; do
+
+done
