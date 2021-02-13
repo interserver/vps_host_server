4be56f24f8cc76e45b1d4010cc0f83d0ed723c82
Joe Huss <detain@interserver.net>
2021-02-13 04:45:49 -0500

    updates to update_virtuozzo script improving its checks and adding a mail to supprot when it cannot find the vzpkg program including the hostname

--- update_virtuozzo.sh
+++ update_virtuozzo.sh
@@ -11,10 +11,14 @@ function age() {
 }
 
 yum upgrade -y;
-vzpkg update metadata;
-vzpkg list -O | awk '{ print $1 }' | xargs -n 1 vzpkg fetch -O;
-vzlist -a -H | awk '{ print $1 }' |xargs -n 1 vzpkg update;
-if [ ! -e ".cron_weekly.age" ] || [ $(age .cron_weekly.age) -ge 604800 ]; then
-  vzpkg update cache --update-cache;
-  touch .cron_weekly.age;
+if [ "$(which vzpkg)" = "" ]; then
+  echo "Cannot find vzpkg package for update_virtuozzo.sh script on $HOSTNAME" | mail support@interserver.net
+else
+  vzpkg update metadata;
+  vzpkg list -O | awk '{ print $1 }' | xargs -n 1 vzpkg fetch -O;
+  vzlist -a -H | awk '{ print $1 }' |xargs -n 1 vzpkg update;
+  if [ ! -e ".cron_weekly.age" ] || [ $(age .cron_weekly.age) -ge 604800 ]; then
+	vzpkg update cache --update-cache;
+	touch .cron_weekly.age;
+  fi
 fi
