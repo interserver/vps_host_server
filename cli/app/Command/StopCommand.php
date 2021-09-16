7932d3b072b744fe18c8cc4edcefb327a0c4def7
Joe Huss <detain@interserver.net>
2021-09-16 14:04:21 -0400

    refactored some of hte generic vps methods into thier own static class changing them to static methods for easy reference

--- cli/app/Command/StopCommand.php
+++ cli/app/Command/StopCommand.php
@@ -6,6 +6,7 @@ use CLIFramework\Formatter;
 use CLIFramework\Logger\ActionLogger;
 use CLIFramework\Debug\LineIndicator;
 use CLIFramework\Debug\ConsoleDebug;
+use App\Vps;
 
 class StopCommand extends Command {
     public $error = 0;
@@ -20,71 +21,41 @@ class StopCommand extends Command {
 	}
 
 	public function execute($hostname) {
-		if (!$this->isVirtualHost()) {
+		if (!Vps::isVirtualHost()) {
 			$this->getLogger()->writeln("This machine does not appear to have any virtualization setup installed.");
 			$this->getLogger()->writeln("Check the help to see how to prepare a virtualization environment.");
 			return 1;
 		}
-		if (!$this->vpsExists($hostname)) {
+		if (!Vps::vpsExists($hostname)) {
 			$this->getLogger()->writeln("The VPS '{$hostname}' you specified does not appear to exist, check the name and try again.");
 			return 1;
 		}
 		$this->stopVps($hostname);
 	}
 
-    public function getInstalledVirts() {
-		$found = [];
-		foreach ($this->virtBins as $virt => $virtBin) {
-			if (file_exists($virtBin)) {
-				$found[] = $virt;
-			}
-		}
-		return $found;
-    }
-
-    public function isVirtualHost() {
-		$virts = $this->getInstalledVirts();
-		return count($virts) > 0;
-    }
-
-    public function getRunningVps() {
-		return explode("\n", trim(`virsh list --name`));
-    }
-
-    public function isVpsRunning($hostname) {
-		return in_array($hostname, $this->getRunningVps());
-    }
-
-	public function vpsExists($hostname) {
-		passthru('/usr/bin/virsh dominfo '.$hostname.' >/dev/null 2>&1', $return);
-		return $return == 0;
-	}
-
 	public function stopVps($hostname) {
-		if ($this->error == 0) {
-			$this->getLogger()->info2('Stopping the VPS');
-			$this->getLogger()->indent();
-			$this->getLogger()->info2('Sending Softwawre Power-Off');
-			echo `/usr/bin/virsh stop {$hostname};`;
-			$stopped = false;
-			$waited = 0;
-			$maxWait = 240;
-			$sleepTime = 10;
-			$continue = true;
-			while ($waited <= $maxWait && $stopped == false) {
-				if ($this->isVpsRunning($hostname)) {
-					$this->getLogger()->info2('VPS is still running, waiting '.$sleepTime.' seconds (waited '.$waited.'/'.$maxWait.')');
-					sleep($sleepTime);
-					$waited += $sleepTime;
-				} else {
-					$stopped = true;
-				}
+		$this->getLogger()->info2('Stopping the VPS');
+		$this->getLogger()->indent();
+		$this->getLogger()->info2('Sending Softwawre Power-Off');
+		echo `/usr/bin/virsh stop {$hostname};`;
+		$stopped = false;
+		$waited = 0;
+		$maxWait = 240;
+		$sleepTime = 10;
+		$continue = true;
+		while ($waited <= $maxWait && $stopped == false) {
+			if ($this->isVpsRunning($hostname)) {
+				$this->getLogger()->info2('VPS is still running, waiting '.$sleepTime.' seconds (waited '.$waited.'/'.$maxWait.')');
+				sleep($sleepTime);
+				$waited += $sleepTime;
+			} else {
+				$stopped = true;
 			}
-			if ($stopped === false) {
-				$this->getLogger()->info2('Sending Hardware Power-Off');
-				echo `/usr/bin/virsh destroy {$hostname};`;
-			}
-			$this->getLogger()->unIndent();
 		}
+		if ($stopped === false) {
+			$this->getLogger()->info2('Sending Hardware Power-Off');
+			echo `/usr/bin/virsh destroy {$hostname};`;
+		}
+		$this->getLogger()->unIndent();
 	}
 }
