652a355d95ab1a701a886766b17215f35a650e9b
Joe Huss <detain@interserver.net>
2021-09-17 09:02:34 -0400

    added the change-hostname command

--- cli/app/Command/ChangeHostnameCommand.php
+++ cli/app/Command/ChangeHostnameCommand.php
@@ -15,10 +15,11 @@ class ChangeHostnameCommand extends Command {
 
     /** @param \CLIFramework\ArgInfoList $args */
 	public function arguments($args) {
-		$args->add('hostname')->desc('Hostname to use')->isa('string');
+		$args->add('hostname')->desc('Current Hostname')->isa('string');
+		$args->add('newname')->desc('New Hostname')->isa('string');
 	}
 
-	public function execute($hostname) {
+	public function execute($hostname, $newname) {
 		if (!Vps::isVirtualHost()) {
 			$this->getLogger()->error("This machine does not appear to have any virtualization setup installed.");
 			$this->getLogger()->error("Check the help to see how to prepare a virtualization environment.");
@@ -28,33 +29,37 @@ class ChangeHostnameCommand extends Command {
 			$this->getLogger()->error("The VPS '{$hostname}' you specified does not appear to exist, check the name and try again.");
 			return 1;
 		}
+		if (Vps::vpsExists($newname)) {
+			$this->getLogger()->error("The VPS '{$newname}' you specified already exists so we cannot rename '{$hostname}' to it.");
+			return 1;
+		}
 		$pool = Vps::getPoolType();
 		Vps::stopVps($hostname);
-/*
-if [ "$pool" = "zfs" ]; then
-  zfs rename vz/{$hostname} vz/{$param|escapeshellarg};
-else
-  lvrename /dev/vz/{$hostname} vz/{$param|escapeshellarg};
-fi;
-virsh domrename {$hostname} {$param|escapeshellarg};
-virsh dumpxml {$param|escapeshellarg} > vps.xml;
-sed s#"{$hostname}"#{$param|escapeshellarg}#g -i vps.xml;
-virsh define vps.xml;
-rm -fv vps.xml;
-for i in /etc/dhcpd.vps /etc/dhcp/dhcpd.vps /root/cpaneldirect/vps.ipmap /root/cpaneldirect/vps.mainips /root/cpaneldirect/vps.slicemap /root/cpaneldirect/vps.vncmap; do
-  if [ -e $i ]; then
-	sed s#"{$hostname}"#{$param|escapeshellarg}#g -i $i;
-  fi;
-done;
-if [ -e /etc/apt ]; then
-	systemctl restart isc-dhcp-server 2>/dev/null || service isc-dhcp-server restart 2>/dev/null || /etc/init.d/isc-dhcp-server restart 2>/dev/null;
-else
-	systemctl restart dhcpd 2>/dev/null || service dhcpd restart 2>/dev/null || /etc/init.d/dhcpd restart 2>/dev/null;
-fi;
-rm -vf /etc/xinetd.d/{$hostname} /etc/xinetd.d/{$hostname}-spice;
-virsh start {$param|escapeshellarg};
-./vps_refresh_vnc.sh {$param|escapeshellarg};
-*/
+		if ($pool == 'zfs') {
+			echo `zfs rename vz/{$hostname} vz/{$newname}`;
+		} else {
+			echo `lvrename /dev/vz/{$hostname} vz/{$newname}`;
+		}
+		echo `virsh domrename {$hostname} {$newname}`;
+		echo `virsh dumpxml {$newname} > /root/cpaneldirect/vps.xml`;
+		echo `sed s#"{$hostname}"#{$newname}#g -i /root/cpaneldirect/vps.xml`;
+		echo `virsh define /root/cpaneldirect/vps.xml`;
+		echo `rm -fv /root/cpaneldirect/vps.xml`;
+		foreach (['/etc/dhcpd.vps', '/etc/dhcp/dhcpd.vps', '/root/cpaneldirect/vps.ipmap', '/root/cpaneldirect/vps.mainips', '/root/cpaneldirect/vps.slicemap', '/root/cpaneldirect/vps.vncmap'] as $file) {
+			if (file_exists($file)) {
+				$data = file_get_contents($file);
+				$data = str_replace($hostname, $newname, $data);
+				file_put_contents($file, $data);
+			}
+		}
+		if (file_exists('/etc/apt')) {
+			echo `systemctl restart isc-dhcp-server 2>/dev/null || service isc-dhcp-server restart 2>/dev/null || /etc/init.d/isc-dhcp-server restart 2>/dev/null`;
+		} else {
+			echo `systemctl restart dhcpd 2>/dev/null || service dhcpd restart 2>/dev/null || /etc/init.d/dhcpd restart 2>/dev/null`;
+		}
+		echo `rm -vf /etc/xinetd.d/{$hostname} /etc/xinetd.d/{$hostname}-spice`;
+		echo `virsh start {$newname}`;
+		echo `/root/cpaneldirect/vps_refresh_vnc.sh {$newname}`;
 	}
 
 }
