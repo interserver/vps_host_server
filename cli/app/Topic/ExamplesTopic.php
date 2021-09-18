369df3c8679f2e3103f7a1934d6d8ba1e4ea0b94
Joe Huss <detain@interserver.net>
2021-09-18 16:28:43 -0400

    adding some examples

--- cli/app/Topic/ExamplesTopic.php
+++ cli/app/Topic/ExamplesTopic.php
@@ -9,17 +9,21 @@ class ExamplesTopic extends BaseTopic {
 
     public function getContent() {
     	return
-'        system setup
-            system setup info here
+'        Command Examples
+            provirted create vps12345 162.246.19.201 ubuntu12 50 4096 4 p4ssw0rd;
+            provirted create -i 162.246.19.202 -c 70.44.33.193 vps12345 162.246.19.201 ubuntu-20.04 100 4096 4 p4ssw0rd;
+            provirted create --add-ip=208.73.201.161 --add-ip=208.73.201.162 --add-ip=208.73.201.163 \
+                             --client-ip=70.44.33.193 vps12345 208.73.201.160 centos5 100 4096 4 p4ssw0rd;
+            provirted stop vps12345;
+            provirted start vps12345;
+            provirted restart vps12345;
+            provirted setup-vnc vps12345 70.44.33.193;
+            provirted delete vps12345;
+            provirted enable vps12345;
+            provirted destroy vps12345;
 
-        vps install
-            info about the vps installs
-
-        templates
-            templates are stored in /vz/templates or / usually
-
-        storage
-            info here about storage';
+        Contributing
+            Got a great example and feel it should be included? Submit a pull request or issue.';
     }
 
 	//public function getFooter() {}
