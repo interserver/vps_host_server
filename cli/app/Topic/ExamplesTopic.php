f7c7e63d3e1bcc3c94ff588d0fcd29d5ffaba07f
Joe Huss <detain@interserver.net>
2021-09-18 20:55:33 -0400

    added some examples for cd commands

--- cli/app/Topic/ExamplesTopic.php
+++ cli/app/Topic/ExamplesTopic.php
@@ -10,17 +10,22 @@ class ExamplesTopic extends BaseTopic {
     public function getContent() {
     	return
 '        Command Examples
-            provirted create vps12345 162.246.19.201 ubuntu12 50 4096 4 p4ssw0rd;
+            provirted create vps12345 162.246.19.201 ubuntu12 50 4096 4 p4ssw0rd
             provirted create -i 162.246.19.202 -c 70.44.33.193 vps12345 162.246.19.201 ubuntu-20.04 100 4096 4 p4ssw0rd;
             provirted create --add-ip=208.73.201.161 --add-ip=208.73.201.162 --add-ip=208.73.201.163 \
-                             --client-ip=70.44.33.193 vps12345 208.73.201.160 centos5 100 4096 4 p4ssw0rd;
-            provirted stop vps12345;
-            provirted start vps12345;
-            provirted restart vps12345;
-            provirted setup-vnc vps12345 70.44.33.193;
-            provirted delete vps12345;
-            provirted enable vps12345;
-            provirted destroy vps12345;
+                             --client-ip=70.44.33.193 vps12345 208.73.201.160 centos5 100 4096 4 p4ssw0rd
+            provirted stop vps12345
+            provirted start vps12345
+            provirted restart vps12345
+            provirted setup-vnc vps12345 70.44.33.193
+            provirted delete vps12345
+            provirted enable vps12345
+            provirted destroy vps12345
+            provirted disable-cd vps1
+            provirted enable-cd vps1 "https://mirror.trouble-free.net/iso/KNOPPIX_V7.2.0CD-2013-06-16-EN.iso"
+            provirted eject-cd vps1
+            provirted insert-cd vps1 "https://mirror.trouble-free.net/iso/KNOPPIX_V7.2.0CD-2013-06-16-EN.iso"
+
 
         Contributing
             Got a great example and feel it should be included? Submit a pull request or issue.';
