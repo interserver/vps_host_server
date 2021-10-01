0b8968f143dfbe2ec0c6a9d6ae40419eff3d51bb
Joe Huss <detain@interserver.net>
2021-10-01 00:55:20 -0400

    updates

--- cli/app/Command/StopCommand.php
+++ cli/app/Command/StopCommand.php
@@ -22,7 +22,7 @@ class StopCommand extends Command {
 
     /** @param \CLIFramework\ArgInfoList $args */
 	public function arguments($args) {
-		$args->add('vzid')->desc('VPS id/name to use')->isa('string');
+		$args->add('vzid')->desc('VPS id/name to use')->isa('string');//->validValues([Vps::class, 'getAllVps']);
 	}
 
 	public function execute($vzid) {
