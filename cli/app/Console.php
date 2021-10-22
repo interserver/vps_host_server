f31ed2302f56e1fece3ca6d7d498443db3da3304
Joe Huss <detain@interserver.net>
2021-10-22 04:51:52 -0400

    minor tweak to the grouping of commands

--- cli/app/Console.php
+++ cli/app/Console.php
@@ -15,7 +15,7 @@ class Console extends Application
     	$this->commandGroup('Power', ['stop', 'start', 'restart']);
     	$this->commandGroup('Provisioning', ['config', 'create', 'destroy', 'enable', 'delete', 'backup', 'restore', 'test']);
     	$this->commandGroup('Maintanance', ['change-hostname', 'change-timezone', 'install-cpanel', 'reset-password', 'update', 'cd',
-        	'block-smtp', 'add-ip', 'remove-ip', 'change-ip', 'setup-vnc']);
+        	'block-smtp', 'add-ip', 'remove-ip', 'change-ip', 'setup-vnc', 'rebuild-dhcp']);
         $this->commandGroup("Development Commands", ['generate-internals', 'source'])->setId('dev');
     	$this->topic('basic');
     	$this->topic('examples');
