0096ebd34306639a7b192c63cb2abfe334c3b828
Joe Huss <detain@interserver.net>
2021-11-23 22:02:58 -0500

    updated vps/qs cron enabling the host-info replacement for qs/vps_update_info again after figuring out the filedescriptor warning from lvdisplay

--- vps_cron.sh
+++ vps_cron.sh
@@ -56,8 +56,8 @@ if [ $old_cron -eq 1 ]; then
 			#$dir/cpu_usage_updater.sh 2>$dir/cron.cpu_usage >&2 &
 			$dir/provirted.phar cron cpu-usage 2>$dir/cron.cpu_usage >&2 &
 		fi;
-		$dir/vps_update_info.php >> $log 2>&1
-		#$dir/provirted.phar cron host-info >> $log 2>&1
+		#$dir/vps_update_info.php >> $log 2>&1
+		$dir/provirted.phar cron host-info >> $log 2>&1
 		curl -s --connect-timeout 60 --max-time 600 -k -d action=get_new_vps $url 2>/dev/null > $dir/cron.cmd;
 		if [ "$(cat $dir/cron.cmd)" != "" ]; then
 			echo "Get New VPS Running:	$(cat $dir/cron.cmd)" >> $log;
