0d812f7d8868ac2365ed0d71351e724701fa730a
Joe Huss <detain@interserver.net>
2021-12-03 23:57:50 -0500

    updates to the virt-builder commands splitting the commands up over several lines for easier reading / management

--- templates/build_kvm.sh
+++ templates/build_kvm.sh
@@ -37,25 +37,42 @@ for i in ${templates}; do
 	label="$(echo "$i"|sed s#"^[^ ]* *[^ ]* *"#""#g)"
 	os="$(echo "$tag"|cut -d- -f1)"
 	version="$(echo "$tag"|cut -d- -f2-)"
-	cmd="virt-builder -v --network --colors -m 2048 --smp 8 --format ${format} --arch ${arch} -o ${tag}.${ext} --edit '/etc/ssh/sshd_config: s{^#PermitRootLogin}{PermitRootLogin}; s{^PermitRootLogin.*$}{PermitRootLogin yes};' --root-password 'password:interserver123' --ssh-inject 'root:string:from=\"66.45.228.251\" ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAgEAvuKNsgUCyIoXcpiYkfOikuzlY1TlGGKgU6jMqEu/abStxgncwIX6eV19F5WAl8WYFbpOaolIFAR1Slxd2t7FuSK9B9BGqLNYdhwOLd75EPK71gAbnE2proZvOkuVSNb6Eq6ZHzlWiRVISXZyeGfMiJWr8/BDaIOJQaUUJ5/PcLOcuvpQxqslCninf2usswNQ6feRgYRbebgY6ydBuWpvf1moTxBogAVkh5cvdmGFmFlK5L2OMnJJgfwaLHkE//F60CU5LTaZPMuK/DEM0TyPBKdNAR+4oNiw3NdX/CzCq8VPZyjaIpNkGCsMgZGC4gYcY7TXSOek+870ONGaPKKcQJVJe3IE48zeGQSAUe4FoZwoGVvOMuyM1Lh7986Q6Co8zLiGUOfvfD08kmsCtRuRhA04VigVKEEY/b1zS8T4wC1slb77HhbTL+Q0rF84rh0m0pZ2BFUDwpM64shsTfy7JVr8akN7A68UMA5yT/G7U0o3YsZW/Q0dmu/KaOv/s1sJ1Fhie/om5qsg31qZr1R9GyiOCq3qB5ZC8J8sH3ZKhHEH5ulO6nf6J02WIYJJUuIu2CSqlsvOWNwgp5z1H2T0HA407cetqRcGH+4ymBvXiLcPZTRi5wO/QGBX1NvyNP2MFaASeNm+EIvWXlQVVXnHIT5UdPLYHVv+L+YHkOT185k= root@tech.trouble-free.net'  --hostname=${os}.is.cc --firstboot-install nano,psmisc,wget,rsync,net-tools"
+	cmd="virt-builder --network --colors -m 2048 --smp 8 --format ${format} --arch ${arch} -o ${tag}.${ext}"
+	cmd="${cmd} --edit '/etc/ssh/sshd_config: s{^#PermitRootLogin}{PermitRootLogin}; s{^PermitRootLogin.*$}{PermitRootLogin yes};'"
+	cmd="${cmd} --root-password 'password:interserver123'"
+	cmd="${cmd} --ssh-inject 'root:string:from=\"66.45.228.251\" ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAgEAvuKNsgUCyIoXcpiYkfOikuzlY1TlGGKgU6jMqEu/abStxgncwIX6eV19F5WAl8WYFbpOaolIFAR1Slxd2t7FuSK9B9BGqLNYdhwOLd75EPK71gAbnE2proZvOkuVSNb6Eq6ZHzlWiRVISXZyeGfMiJWr8/BDaIOJQaUUJ5/PcLOcuvpQxqslCninf2usswNQ6feRgYRbebgY6ydBuWpvf1moTxBogAVkh5cvdmGFmFlK5L2OMnJJgfwaLHkE//F60CU5LTaZPMuK/DEM0TyPBKdNAR+4oNiw3NdX/CzCq8VPZyjaIpNkGCsMgZGC4gYcY7TXSOek+870ONGaPKKcQJVJe3IE48zeGQSAUe4FoZwoGVvOMuyM1Lh7986Q6Co8zLiGUOfvfD08kmsCtRuRhA04VigVKEEY/b1zS8T4wC1slb77HhbTL+Q0rF84rh0m0pZ2BFUDwpM64shsTfy7JVr8akN7A68UMA5yT/G7U0o3YsZW/Q0dmu/KaOv/s1sJ1Fhie/om5qsg31qZr1R9GyiOCq3qB5ZC8J8sH3ZKhHEH5ulO6nf6J02WIYJJUuIu2CSqlsvOWNwgp5z1H2T0HA407cetqRcGH+4ymBvXiLcPZTRi5wO/QGBX1NvyNP2MFaASeNm+EIvWXlQVVXnHIT5UdPLYHVv+L+YHkOT185k= root@tech.trouble-free.net'"
+	cmd="${cmd} --hostname=${os}.is.cc"
+	cmd="${cmd} --firstboot-install nano,psmisc,wget,rsync,net-tools"
 	if [ "$arch" != "x86_64" ]; then
 		continue
 	fi;
 	case $os in
 	"centos")
-		h=mirror.trouble-free.net cmd="${cmd} --edit '/etc/yum.repos.d/CentOS-Base.repo: s{^mirrorlist=}{#mirrorlist=}; s{^#baseurl=}{baseurl=}; s{mirror.centos.org}{mirror.trouble-free.net};' --append-line '/etc/hosts:$(host $h|grep "has address"|head -n 1|cut -d" " -f4) $h' --append-line '/etc/sysconfig/network-scripts/ifcfg-eth0:DEVICE=eth0' --selinux-relabel --update";;
+		h=mirror.trouble-free.net
+		cmd="${cmd} --edit '/etc/yum.repos.d/CentOS-Base.repo: s{^mirrorlist=}{#mirrorlist=}; s{^#baseurl=}{baseurl=}; s{mirror.centos.org}{mirror.trouble-free.net};'"
+		cmd="${cmd} --append-line '/etc/hosts:$(host $h|grep "has address"|head -n 1|cut -d" " -f4) $h'";
+		cmd="${cmd} --append-line '/etc/sysconfig/network-scripts/ifcfg-eth0:DEVICE=eth0'";
+		cmd="${cmd} --selinux-relabel"
+		cmd="${cmd} --update";;
 	"fedora")
 		for h in mirrors.fedoraproject.org dl.fedoraproject.org mirrors.rit.edu mirrors.kernel.org; do
 			cmd="${cmd} --append-line '/etc/hosts:$(host $h|grep "has address"|head -n 1|cut -d" " -f4) $h'";
 		done;
-		cmd="${cmd} --selinux-relabel --update";;
+		cmd="${cmd} --selinux-relabel"
+		cmd="${cmd} --update";;
 	"opensuse")
-		h=download.opensuse.org cmd="${cmd} --append-line '/etc/hosts:$(host $h|grep "has address"|head -n 1|cut -d" " -f4) $h'";
+		h=download.opensuse.org
+		cmd="${cmd} --append-line '/etc/hosts:$(host $h|grep "has address"|head -n 1|cut -d" " -f4) $h'";
 		if [ "$(echo ${version:0:1}|grep "[0-9]")" != "" ]; then
 			cmd="${cmd} --update";
 		fi;;
 	"scientificlinux")
-		h=ftp.scientificlinux.org cmd="${cmd} --edit '/etc/yum.repos.d/sl-other.repo: s{linux/scientific}{linux/scientific/obsolete};' --edit '/etc/yum.repos.d/sl6x.repo: s{linux/scientific}{linux/scientific/obsolete};' --edit '/etc/yum.repos.d/sl.repo: s{linux/scientific}{linux/scientific/obsolete};' --append-line '/etc/hosts:$(host $h|grep "has address"|head -n 1|cut -d" " -f4) $h' --update";;
+		h=ftp.scientificlinux.org
+		cmd="${cmd} --edit '/etc/yum.repos.d/sl-other.repo: s{linux/scientific}{linux/scientific/obsolete};'"
+		cmd="${cmd} --edit '/etc/yum.repos.d/sl6x.repo: s{linux/scientific}{linux/scientific/obsolete};'"
+		cmd="${cmd} --edit '/etc/yum.repos.d/sl.repo: s{linux/scientific}{linux/scientific/obsolete};'"
+		cmd="${cmd} --append-line '/etc/hosts:$(host $h|grep "has address"|head -n 1|cut -d" " -f4) $h'"
+		cmd="${cmd} --update";;
 	"debian" | "ubuntu")
 		cmd="${cmd} --firstboot-command 'dpkg-reconfigure openssh-server'";
 		if [ "$version" != "10.04" ] && [ "$version" != "12.04" ] && [ "$version" != "14.04" ]; then
