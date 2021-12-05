fa70404044a9f78bf938110b54e58b9fb9d03663
Joe Huss <detain@interserver.net>
2021-12-05 02:53:18 -0500

    updates to build script for fedora disabling selinux on f<=20 and f>=31

--- templates/build_kvm.sh
+++ templates/build_kvm.sh
@@ -64,13 +64,11 @@ for i in ${templates}; do
 		for h in mirrors.fedoraproject.org dl.fedoraproject.org mirrors.rit.edu mirrors.kernel.org; do
 			cmd="${cmd} --append-line '/etc/hosts:$(host $h|grep "has address"|head -n 1|cut -d" " -f4) $h'";
 		done;
-		if [ $(echo "$version"|sed "s#[^0-9]##g") -gt 20 ]; then
-			cmd="${cmd} --install nano,psmisc,wget,rsync,net-tools"
-			cmd="${cmd} --update";
-		fi
-		if [ "$version" = "31" ]; then
+		if [ $(echo "$version"|sed "s#[^0-9]##g") -le 20 ] || [ $(echo "$version"|sed "s#[^0-9]##g") -ge 31 ]; then
 			cmd="${cmd} --edit '/etc/selinux/config: s/SELINUX=enforcing/SELINUX=disabled/'"
 		else
+			cmd="${cmd} --install nano,psmisc,wget,rsync,net-tools"
+			cmd="${cmd} --update";
 			cmd="${cmd} --selinux-relabel"
 		fi
 		;;
