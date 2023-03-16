f882f12ae0f65e6559703ce25d0dae08e5960744
Joe Huss <detain@interserver.net>
2023-03-16 16:01:45 -0400

    updated set io limit script tow ork with varied block devices

--- set_io_limits.sh
+++ set_io_limits.sh
@@ -3,20 +3,22 @@ IFS="
 "
 eval declare -A adminvps=($(for i in $(curl -s https://mynew.interserver.net/adminvps.php); do echo -n "[$i]=1 ";done))
 eval declare -A runningvps=($(for i in $(virsh list --name); do echo "[$i]=1 ";done))
-for i in $(cat /root/cpaneldirect/vps.slicemap); do
-  v=$(echo $i|cut -d: -f1)
-  s=$(echo $i|cut -d: -f2)
-  iops=$((100 + $((100 * $s))))
-  io=$((100000000 * $s))
-  echo -n "vps $v: slices $s = iops ${iops} + io ${io}, "
-  if [ "${adminvps[$v]}" = "1" ]; then
+for line in $(cat /root/cpaneldirect/vps.slicemap); do
+  vps=$(echo $line|cut -d: -f1)
+  slices=$(echo $line|cut -d: -f2)
+  iops=$((100 + $((100 * $slices))))
+  io=$((100000000 * $slices))
+  echo -n "vps $vps: slices $slices = iops ${iops} + io ${io}, "
+  if [ "${adminvps[$vps]}" = "1" ]; then
     echo "skipping interserver/admin service"
   else
-    echo -n "updating config, "
-    virsh blkdeviotune $v vda --total-iops-sec ${iops} --total-bytes-sec ${io} --config |grep -v "^$"
-    if [ -n ${runningvps[$v]} ]; then
-      echo "updating live"
-      virsh blkdeviotune $v vda --total-iops-sec ${iops} --total-bytes-sec ${io} --live |grep -v "^$"
-    fi
+    for dev in $(virsh domblklist $vps|grep "^ .* */"|cut -d" " -f2); do
+      echo -n "updating $dev config, "
+      virsh blkdeviotune $vps $dev --total-iops-sec ${iops} --total-bytes-sec ${io} --config |grep -v "^$"
+      if [ -n ${runningvps[$vps]} ]; then
+        echo "updating $dev live"
+        virsh blkdeviotune $vps $dev --total-iops-sec ${iops} --total-bytes-sec ${io} --live |grep -v "^$"
+      fi
+    done
   fi
 done
