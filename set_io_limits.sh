b550ab2348afc85b785049f1a30b54b08a7d37ab
Joe Huss <detain@interserver.net>
2023-03-16 16:19:30 -0400

    updates chcking for protected file in set io script

--- set_io_limits.sh
+++ set_io_limits.sh
@@ -9,16 +9,19 @@ for line in $(cat /root/cpaneldirect/vps.slicemap); do
   iops=$((100 + $((100 * $slices))))
   io=$((100000000 * $slices))
   echo -n "vps $vps: slices $slices = iops ${iops} + io ${io}, "
-  if [ "${adminvps[$vps]}" = "1" ]; then
+  if [ -e "/vz/$vps/protected" ]; then
+    echo "skipping protected vps"
+    continue;
+  elif [ "${adminvps[$vps]}" = "1" ]; then
     echo "skipping interserver/admin service"
-  else
-    for dev in $(virsh domblklist $vps|grep "^ .* */"|cut -d" " -f2); do
-      echo -n "updating $dev config, "
-      virsh blkdeviotune $vps $dev --total-iops-sec ${iops} --total-bytes-sec ${io} --config |grep -v "^$"
-      if [ -n ${runningvps[$vps]} ]; then
-        echo "updating $dev live"
-        virsh blkdeviotune $vps $dev --total-iops-sec ${iops} --total-bytes-sec ${io} --live |grep -v "^$"
-      fi
-    done
+    continue;
   fi
+  for dev in $(virsh domblklist $vps|grep "^ .* */"|cut -d" " -f2); do
+    echo -n "updating $dev config, "
+    virsh blkdeviotune $vps $dev --total-iops-sec ${iops} --total-bytes-sec ${io} --config |grep -v "^$"
+    if [ "${runningvps[$vps]}" = "1" ]; then
+      echo "updating $dev live"
+      virsh blkdeviotune $vps $dev --total-iops-sec ${iops} --total-bytes-sec ${io} --live |grep -v "^$"
+    fi
+  done
 done
